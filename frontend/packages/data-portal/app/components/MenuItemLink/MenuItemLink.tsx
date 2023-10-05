c48b4fdd1ce445e0307dc91e9914b269de0ce0b2
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-10-05 03:34:03 -0700

    MenuDropdown + MenuItemLink + MenuItemHeader components

---
+++ frontend/packages/data-portal/app/components/MenuItemLink/MenuItemLink.tsx
@@ -0,0 +1,19 @@
+import { MenuItem } from '@czi-sds/components'
+import { ComponentProps } from 'react'
+
+import { Link } from 'app/components/Link'
+
+import styles from './MenuItemLink.module.css'
+
+export function MenuItemLink({
+  divider,
+  ...props
+}: ComponentProps<typeof Link> & {
+  divider?: boolean
+}) {
+  return (
+    <MenuItem className={styles.menuItem} divider={divider}>
+      <Link className="flex flex-auto !text-sds-body-xs" {...props} />
+    </MenuItem>
+  )
+}
