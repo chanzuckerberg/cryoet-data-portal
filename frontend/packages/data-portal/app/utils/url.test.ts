8c5c447496425d7fd6fa7968af8b5ee830082d8f
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-10-20 13:20:51 -0700

    add createUrl() util

--- frontend/packages/data-portal/app/utils/url.test.ts
+++ frontend/packages/data-portal/app/utils/url.test.ts
@@ -1,4 +1,4 @@
-import { isExternalUrl } from './url'
+import { createUrl, isExternalUrl } from './url'
 
 describe('utils/url', () => {
   describe('isExternalUrl()', () => {
@@ -8,6 +8,28 @@ describe('utils/url', () => {
 
     it('should return true for non-external URLs', () => {
       expect(isExternalUrl('/faq')).toBe(false)
+    })
+  })
+
+  describe('createUrl()', () => {
+    it('should create url object without host', () => {
+      const url = createUrl('/path')
+      expect(url.pathname).toBe('/path')
+      expect(url.host).toBe('tmp.com')
+    })
+
+    it('should create url object with host', () => {
+      const testCases: [string, string?][] = [
+        ['https://example.com/path'],
+        ['/path', 'http://example.com'],
+      ]
+
+      for (const testCase of testCases) {
+        const url = createUrl(...testCase)
+
+        expect(url.pathname).toBe('/path')
+        expect(url.host).toBe('example.com')
+      }
     })
   })
 })
