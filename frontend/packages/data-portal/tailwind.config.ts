34e02af7476b3e4e43a56cf2c311a33f3f6bab65
Kira Evans <contact@kne42.me>
2023-10-26 20:04:48 -0700

    override faulty sds line heights

--- frontend/packages/data-portal/tailwind.config.ts
+++ frontend/packages/data-portal/tailwind.config.ts
@@ -11,6 +11,30 @@ export default {
       maxWidth: {
         content: '1600px',
       },
+
+      // overwrite faulty SDS line heights
+      lineHeight: {
+        // header
+        'sds-header-xxl': '34px',
+        'sds-header-xl': '30px',
+        'sds-header-l': '24px',
+        'sds-header-m': '22px',
+        'sds-header-s': '20px',
+        'sds-header-xs': '18px',
+        'sds-header-xxs': '18px',
+        'sds-header-xxxs': '16px',
+        // body
+        'sds-body-l': '28px',
+        'sds-body-m': '26px',
+        'sds-body-s': '24px',
+        'sds-body-xs': '20px',
+        'sds-body-xxs': '18px',
+        'sds-body-xxxs': '16px',
+        // caps
+        'sds-caps-xxs': '18px',
+        'sds-caps-xxxs': '16px',
+        'sds-caps-xxxxs': '14px',
+      },
     },
   },
   plugins: [],
