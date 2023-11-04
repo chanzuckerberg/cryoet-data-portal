952117fd2107e61153eb3069a5c883e9a9c726b1
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-11-03 17:14:20 -0700

    add query for getting run by id

--- frontend/packages/data-portal/app/routes/runs.$id.tsx
+++ frontend/packages/data-portal/app/routes/runs.$id.tsx
@@ -6,26 +6,23 @@ import { json, LoaderFunctionArgs } from '@remix-run/server-runtime'
 import { gql } from 'app/__generated__'
 import { apolloClient } from 'app/apollo.server'
 import { Demo } from 'app/components/Demo'
-import { RunMetadataDrawer } from 'app/components/Run/RunMetadataDrawer'
+import { RunHeader } from 'app/components/Run'
+import { MAX_PER_PAGE } from 'app/constants/pagination'
 import { useCloseDrawerOnUnmount, useDrawer } from 'app/state/drawer'
 
 const GET_RUN_BY_ID_QUERY = gql(`
-  query GetRunById($id: Int) {
-    runs(where: {id: {_eq: $id}}) {
+  query GetRunById($id: Int, $limit: Int, $offset: Int) {
+    runs(where: { id: { _eq: $id } }) {
+      id
       name
+
       tiltseries {
         acceleration_voltage
+        binning_from_frames
         camera_manufacturer
         camera_model
         data_acquisition_software
-        tilt_axis
-        tilt_max
-        tilt_min
-        tilt_range
-        tilt_series_quality
-        tilt_step
-        tilting_scheme
-        total_flux
+        id
         microscope_additional_info
         microscope_energy_filter
         microscope_image_corrector
@@ -34,38 +31,35 @@ const GET_RUN_BY_ID_QUERY = gql(`
         microscope_phase_plate
         related_empiar_entry
         spherical_aberration_constant
-        id
-        binning_from_frames
+        tilt_axis
+        tilt_max
+        tilt_min
+        tilt_range
+        tilt_series_quality
+        tilt_step
+        tilting_scheme
+        total_flux
       }
 
       dataset {
-        # Dataset dates
-        last_modified_date
-        release_date
-        deposition_date
-
-        # Dataset metadata
-        id
-        title
-        description
-        funding_sources {
-          funding_agency_name
-        }
-
-        # TODO Grant ID
-        related_database_entries
-        dataset_citations
-
-        # Sample and experiments data
-        sample_type
-        organism_name
-        tissue_name
         cell_name
         cell_strain_name
-        # TODO cellular component
-        sample_preparation
+        dataset_citations
+        dataset_publications
+        deposition_date
+        description
         grid_preparation
+        id
+        last_modified_date
+        organism_name
         other_setup
+        related_database_entries
+        related_database_entries
+        release_date
+        sample_preparation
+        sample_type
+        tissue_name
+        title
 
         authors(distinct_on: name) {
           name
@@ -74,14 +68,14 @@ const GET_RUN_BY_ID_QUERY = gql(`
           corresponding_author_status
         }
 
-        authors_with_affiliation: authors(where: {affiliation_name: {_is_null: false}}) {
+        authors_with_affiliation: authors(where: { affiliation_name: { _is_null: false } }) {
           name
           affiliation_name
         }
 
-        # publication info
-        related_database_entries
-        dataset_publications
+        funding_sources {
+          funding_agency_name
+        }
       }
 
       tomogram_voxel_spacings(limit: 1) {
@@ -90,8 +84,8 @@ const GET_RUN_BY_ID_QUERY = gql(`
           fiducial_alignment_status
           name
           processing
-          reconstruction_method
           processing_software
+          reconstruction_method
           reconstruction_software
           size_x
           size_y
@@ -99,11 +93,55 @@ const GET_RUN_BY_ID_QUERY = gql(`
           voxel_spacing
         }
       }
+
+      annotation_table: tomogram_voxel_spacings {
+        annotations(limit: $limit, offset: $offset) {
+          confidence_precision
+          confidence_recall
+          object_count
+          object_name
+
+          authors {
+            name
+          }
+        }
+      }
+
+      tomogram_stats: tomogram_voxel_spacings {
+        annotations {
+          object_name
+        }
+
+        annotations_aggregate {
+          aggregate {
+            count
+          }
+        }
+
+        tomograms {
+          processing
+        }
+
+        tomograms_aggregate {
+          aggregate {
+            count
+          }
+        }
+      }
+
+      tiltseries_aggregate {
+        aggregate {
+          count
+          avg {
+            tilt_series_quality
+          }
+        }
+      }
     }
   }
 `)
 
-export async function loader({ params }: LoaderFunctionArgs) {
+export async function loader({ request, params }: LoaderFunctionArgs) {
   const id = params.id ? +params.id : NaN
 
   if (Number.isNaN(+id)) {
@@ -113,10 +151,15 @@ export async function loader({ params }: LoaderFunctionArgs) {
     })
   }
 
+  const url = new URL(request.url)
+  const page = +(url.searchParams.get('page') ?? '1')
+
   const { data } = await apolloClient.query({
     query: GET_RUN_BY_ID_QUERY,
     variables: {
       id: +id,
+      limit: MAX_PER_PAGE,
+      offset: (page - 1) * MAX_PER_PAGE,
     },
   })
 
@@ -131,22 +174,21 @@ export async function loader({ params }: LoaderFunctionArgs) {
 }
 
 export default function RunByIdPage() {
+  useCloseDrawerOnUnmount()
   const params = useParams()
   const drawer = useDrawer()
-  useCloseDrawerOnUnmount()
 
   return (
-    <>
-      <Demo>
-        <div className="flex flex-col gap-sds-xl">
-          <span className="text-5xl">Run Page ID = {params.id}</span>
+    <div className="flex flex-col flex-auto">
+      <RunHeader />
 
-          <button onClick={drawer.toggle} type="button">
-            Toggle Drawer
-          </button>
-        </div>
+      <Demo>
+        <span className="text-5xl">Run Page ID = {params.id}</span>
+
+        <button onClick={drawer.toggle} type="button">
+          Toggle Drawer
+        </button>
       </Demo>
-      <RunMetadataDrawer />
-    </>
+    </div>
   )
 }
