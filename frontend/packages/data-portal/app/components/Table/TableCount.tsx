d598430b55efea079b4fceadea7b1243ca983ba1
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-11-17 16:31:27 -0800

    add skeleton loader to table count

--- frontend/packages/data-portal/app/components/Table/TableCount.tsx
+++ frontend/packages/data-portal/app/components/Table/TableCount.tsx
@@ -1,3 +1,6 @@
+import Skeleton from '@mui/material/Skeleton'
+
+import { useIsLoading } from 'app/hooks/useIsLoading'
 import { i18n } from 'app/i18n'
 
 export function TableCount({
@@ -9,9 +12,15 @@ export function TableCount({
   totalCount: number
   type: string
 }) {
+  const { isLoadingDebounced } = useIsLoading()
+
   return (
-    <p className="text-sds-body-xs text-sds-gray-500 whitespace-nowrap">
-      {i18n.filterCount(filteredCount, totalCount, type)}
+    <p className="text-sds-body-xs text-sds-gray-500 whitespace-nowrap max-w-[80px]">
+      {isLoadingDebounced ? (
+        <Skeleton className="max-w-" variant="text" />
+      ) : (
+        i18n.filterCount(filteredCount, totalCount, type)
+      )}
     </p>
   )
 }
