710aba74f94478015ab7f789d367bde4117a7dc6
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-11-17 16:31:27 -0800

    update useDatasets to return more data

--- frontend/packages/data-portal/app/routes/browse-data.datasets.tsx
+++ frontend/packages/data-portal/app/routes/browse-data.datasets.tsx
@@ -48,14 +48,34 @@ const GET_DATASETS_DATA_QUERY = gql(`
       }
     }
 
-    filtered_datasets_aggregate: datasets_aggregate(
-      where: {
-        title: { _ilike: $query },
-      },
-    ) {
+    filtered_datasets_aggregate: datasets_aggregate(where: $filter) {
       aggregate {
         count
       }
+    }
+
+    organism_names: datasets(distinct_on:organism_name) {
+      organism_name
+    }
+
+    camera_manufacturers: tiltseries(distinct_on: camera_manufacturer) {
+      camera_manufacturer
+    }
+
+    reconstruction_methods: tomograms(distinct_on: reconstruction_method) {
+      reconstruction_method
+    }
+
+    reconstruction_softwares:tomograms(distinct_on: reconstruction_software) {
+      reconstruction_software
+    }
+
+    object_names: annotations(distinct_on: object_name) {
+      object_name
+    }
+
+    object_shape_types: annotations(distinct_on: shape_type) {
+      shape_type
     }
   }
 `)
