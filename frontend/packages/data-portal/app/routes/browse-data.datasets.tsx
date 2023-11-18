b5840d4aa8960aa97b12176af5dedae511ec4ebd
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-11-17 16:31:27 -0800

    integrate UI filter state with backend

--- frontend/packages/data-portal/app/routes/browse-data.datasets.tsx
+++ frontend/packages/data-portal/app/routes/browse-data.datasets.tsx
@@ -1,14 +1,24 @@
+/* eslint-disable no-underscore-dangle */
+
 import { CellHeaderDirection } from '@czi-sds/components'
 import { json, LoaderFunctionArgs } from '@remix-run/node'
+import { isNumber } from 'lodash-es'
+import { match } from 'ts-pattern'
 
 import { gql } from 'app/__generated__'
-import { Order_By } from 'app/__generated__/graphql'
+import { Datasets_Bool_Exp, Order_By } from 'app/__generated__/graphql'
 import { apolloClient } from 'app/apollo.server'
 import { DatasetTable } from 'app/components/BrowseData'
-import { FilterPanel } from 'app/components/FilterPanel'
+import { DatasetFilter } from 'app/components/DatasetFilter'
 import { TablePageLayout } from 'app/components/TablePageLayout'
 import { MAX_PER_PAGE } from 'app/constants/pagination'
+import { DEFAULT_TILT_MAX, DEFAULT_TILT_MIN } from 'app/constants/tiltSeries'
+import {
+  DatasetFilterState,
+  getDatasetFilter,
+} from 'app/hooks/useDatasetFilter'
 import { useDatasets } from 'app/hooks/useDatasets'
+import { getSelfCreatingObject } from 'app/utils/proxy'
 
 const GET_DATASETS_DATA_QUERY = gql(`
   query GetDatasetsData(
@@ -16,14 +26,13 @@ const GET_DATASETS_DATA_QUERY = gql(`
     $offset: Int,
     $order_by_dataset: order_by,
     $query: String,
+    $filter: datasets_bool_exp,
   ) {
     datasets(
       limit: $limit,
       offset: $offset,
       order_by: { title: $order_by_dataset },
-      where: {
-        title: { _ilike: $query },
-      },
+      where: $filter
     ) {
       id
       title
@@ -80,6 +89,151 @@ const GET_DATASETS_DATA_QUERY = gql(`
   }
 `)
 
+function getTiltValue(value: string | null) {
+  if (value && !Number.isNaN(+value)) {
+    return +value
+  }
+
+  return null
+}
+
+function getFilter(datasetFilter: DatasetFilterState) {
+  const filter = getSelfCreatingObject<Datasets_Bool_Exp>()
+
+  // Included contents filters
+  // Ground truth filter
+  if (datasetFilter.includedContents.isGroundTruthEnabled) {
+    filter.runs.tomogram_voxel_spacings.annotations.ground_truth_status._eq =
+      true
+  }
+
+  // Available files filter
+  datasetFilter.includedContents.availableFiles.forEach((file) =>
+    match(file)
+      .with('raw-frames', () => {
+        filter.runs.tiltseries.binning_from_frames._gt = 0
+      })
+      .with('tilt-series', () => {
+        filter.runs.tiltseries_aggregate.count.predicate._gt = 0
+      })
+      .with('tilt-series-alignment', () => {
+        filter.runs.tiltseries.https_alignment_file._is_null = false
+      })
+      .with('tomogram', () => {
+        filter.runs.tomogram_voxel_spacings.tomograms_aggregate.count.predicate._gt = 0
+      })
+      .exhaustive(),
+  )
+
+  // Number of runs filter
+  if (datasetFilter.includedContents.numberOfRuns) {
+    const runCount = +datasetFilter.includedContents.numberOfRuns.slice(1)
+    filter.runs_aggregate.count.predicate._gte = runCount
+  }
+
+  // Id filters
+
+  // Portal ID filter
+  const portalId = +(datasetFilter.ids.portal ?? Number.NaN)
+  if (!Number.isNaN(portalId) && portalId > 0) {
+    filter.id._eq = portalId
+  }
+
+  // Empiar filter
+  const empiarId = datasetFilter.ids.empiar
+  if (empiarId) {
+    filter.dataset_publications._like = `%EMPIAR-${empiarId}%`
+  }
+
+  // EMDB filter
+  const emdbId = datasetFilter.ids.emdb
+  if (emdbId) {
+    filter.dataset_publications._like = `%EMD-${emdbId}%`
+  }
+
+  // Author filters
+
+  // Author name filter
+  if (datasetFilter.author.name) {
+    filter.authors.name._ilike = `%${datasetFilter.author.name}%`
+  }
+
+  // Author Orcid filter
+  if (datasetFilter.author.orcid) {
+    filter.authors.orcid._ilike = `%${datasetFilter.author.orcid}%`
+  }
+
+  // Sample and experiment condition filters
+  const { organismNames } = datasetFilter.sampleAndExperimentConditions
+
+  // Organism name filter
+  if (organismNames.length > 0) {
+    filter.organism_name._in = organismNames
+  }
+
+  // Hardware filters
+  // Camera manufacturer filter
+  if (datasetFilter.hardware.cameraManufacturer) {
+    filter.runs.tiltseries.camera_manufacturer._eq =
+      datasetFilter.hardware.cameraManufacturer
+  }
+
+  // Tilt series metadata filters
+  let tiltMin = getTiltValue(datasetFilter.tiltSeries.min)
+  let tiltMax = getTiltValue(datasetFilter.tiltSeries.max)
+
+  if (isNumber(tiltMin) && !isNumber(tiltMax)) {
+    tiltMax = DEFAULT_TILT_MAX
+  }
+
+  if (!isNumber(tiltMin) && isNumber(tiltMax)) {
+    tiltMin = DEFAULT_TILT_MIN
+  }
+
+  // Tilt range filter
+  if (tiltMin && tiltMax) {
+    filter.runs.tiltseries.tilt_min._gte = tiltMin
+    filter.runs.tiltseries.tilt_max._lte = tiltMax
+  }
+
+  // Tomogram metadata filters
+  if (datasetFilter.tomogram.fiducialAlignmentStatus) {
+    filter.runs.tomogram_voxel_spacings.tomograms.fiducial_alignment_status._eq =
+      datasetFilter.tomogram.fiducialAlignmentStatus === 'true'
+        ? 'FIDUCIAL'
+        : 'NON_FIDUCIAL'
+  }
+
+  // Reconstruction method filter
+  if (datasetFilter.tomogram.reconstructionMethod) {
+    filter.runs.tomogram_voxel_spacings.tomograms.reconstruction_method._eq =
+      datasetFilter.tomogram.reconstructionMethod
+  }
+
+  // Reconstruction software filter
+  if (datasetFilter.tomogram.reconstructionSoftware) {
+    filter.runs.tomogram_voxel_spacings.tomograms.reconstruction_software._eq =
+      datasetFilter.tomogram.reconstructionSoftware
+  }
+
+  // Annotation filters
+  const { objectNames, objectShapeTypes } = datasetFilter.annotation
+
+  // Object names filter
+  if (objectNames.length > 0) {
+    filter.runs.tomogram_voxel_spacings.annotations.object_name._in =
+      objectNames
+  }
+
+  // Object shape type filter
+  if (objectShapeTypes.length > 0) {
+    filter.runs.tomogram_voxel_spacings.annotations.shape_type._in =
+      objectShapeTypes
+  }
+
+  return filter
+}
+
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url)
   const page = +(url.searchParams.get('page') ?? '1')
@@ -101,6 +255,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
       offset: (page - 1) * MAX_PER_PAGE,
       order_by_dataset: orderBy,
       query: `%${search}%`,
+      filter: getFilter(getDatasetFilter(url.searchParams)),
     },
   })
 
@@ -113,7 +268,7 @@ export default function BrowseDatasetsPage() {
   return (
     <TablePageLayout
       filteredCount={filteredDatasetCount}
-      filterPanel={<FilterPanel />}
+      filters={<DatasetFilter />}
       table={<DatasetTable />}
       totalCount={datasetCount}
     />
