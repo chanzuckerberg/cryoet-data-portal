77b620af3a9b6764fca4a49f946c6afab06a277b
Kira Evans <contact@kne42.me>
2023-12-06 15:01:40 -0800

    fix content fetch for legal pages (#224)

--- frontend/packages/data-portal/app/routes/data-submission-policy.tsx
+++ frontend/packages/data-portal/app/routes/data-submission-policy.tsx
@@ -1,8 +1,8 @@
 import { MdxContent } from 'app/components/MDX'
-import { getLocalFileContent } from 'app/utils/repo.server'
+import { getRepoFileContentResponse } from 'app/utils/repo.server'
 
 export async function loader() {
-  return getLocalFileContent('website-docs/data-submission-policy.mdx')
+  return getRepoFileContentResponse('website-docs/data-submission-policy.mdx')
 }
 
 export default function DataSubmissionPolicyPage() {
--- frontend/packages/data-portal/app/routes/privacy.tsx
+++ frontend/packages/data-portal/app/routes/privacy.tsx
@@ -1,8 +1,8 @@
 import { MdxContent } from 'app/components/MDX'
-import { getLocalFileContent } from 'app/utils/repo.server'
+import { getRepoFileContentResponse } from 'app/utils/repo.server'
 
 export async function loader() {
-  return getLocalFileContent('website-docs/privacy-policy.mdx')
+  return getRepoFileContentResponse('website-docs/privacy-policy.mdx')
 }
 
 export default function PrivacyPage() {
