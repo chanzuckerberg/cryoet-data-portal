65c16a1964c8095eb8df23c5c438ee1e727458e5
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-12-12 17:52:46 -0800

    add LOCALHOST_PLAUSIBLE_TRACKING env

--- frontend/packages/data-portal/app/context/Environment.context.ts
+++ frontend/packages/data-portal/app/context/Environment.context.ts
@@ -1,13 +1,14 @@
 import { createContext, useContext } from 'react'
 
 export type EnvironmentContextValue = Required<
-  Pick<NodeJS.ProcessEnv, 'API_URL' | 'ENV'>
+  Pick<NodeJS.ProcessEnv, 'API_URL' | 'ENV' | 'LOCALHOST_PLAUSIBLE_TRACKING'>
 >
 
 export const ENVIRONMENT_CONTEXT_DEFAULT_VALUE: EnvironmentContextValue = {
   API_URL:
     'https://graphql-cryoet-api.cryoet.staging.si.czi.technology/v1/graphql',
   ENV: 'local',
+  LOCALHOST_PLAUSIBLE_TRACKING: 'false',
 }
 
 export const EnvironmentContext = createContext<EnvironmentContextValue>(
