b4db4b4c9ae436d3e6abb9428e80419e8e669e98
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-12-14 23:50:02 -0800

    add meets all label #186

--- frontend/packages/data-portal/app/components/DatasetFilter/IncludedContentsFilterSection.tsx
+++ frontend/packages/data-portal/app/components/DatasetFilter/IncludedContentsFilterSection.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import {
   BooleanFilter,
@@ -29,6 +29,9 @@ const AVAILABLE_FILES_OPTIONS: AvailableFilesFilterOption[] = [
   { value: 'tomogram', label: i18n.tomogram },
 ]
 
+const AVAILABLE_FILES_CLASS_NAME = 'select-available-files'
+const MEETS_ALL_LABEL_ID = 'meets-all'
+
 export function IncludedContentsFilterSection() {
   const {
     updateValue,
@@ -58,6 +61,32 @@ export function IncludedContentsFilterSection() {
 
   const { t } = useI18n()
 
+  const [showMeetsAll, setShowMeetsAll] = useState(availableFiles.length > 0)
+
+  // Really hacky way to get a label to show above the selected chips for the
+  // SDS ComplexFilter component. We need to do this because the SDS does not
+  // yet provide a way to add a label.
+  // TODO Update upstream component to support chip labels
+  useEffect(() => {
+    const meetsAllNode = document.getElementById(
+      MEETS_ALL_LABEL_ID,
+    ) as HTMLSpanElement | null
+
+    if (showMeetsAll && !meetsAllNode) {
+      const filterButtonNode = document.querySelector(
+        `.${AVAILABLE_FILES_CLASS_NAME} > button`,
+      )
+
+      const meetsAll = document.createElement('div')
+      meetsAll.id = MEETS_ALL_LABEL_ID
+      meetsAll.textContent = t('meetsAll')
+
+      filterButtonNode?.insertAdjacentElement('afterend', meetsAll)
+    } else if (!showMeetsAll && meetsAllNode) {
+      meetsAllNode.remove()
+    }
+  }, [showMeetsAll, t])
+
   return (
     <FilterSection title={i18n.includedContents}>
       <BooleanFilter
@@ -73,9 +102,13 @@ export function IncludedContentsFilterSection() {
         options={AVAILABLE_FILES_OPTIONS}
         value={availableFilesOptions}
         label={i18n.availableFiles}
-        onChange={(options) => updateValue(QueryParams.AvailableFiles, options)}
+        onChange={(options) => {
+          setShowMeetsAll((options?.length ?? 0) > 0)
+          updateValue(QueryParams.AvailableFiles, options)
+        }}
         title={`${t('resultsMustIncludeAllFileTypes')}:`}
         popperClassName="max-w-[244px]"
+        className={AVAILABLE_FILES_CLASS_NAME}
       />
 
       <SelectFilter
