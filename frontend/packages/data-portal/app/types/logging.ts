e9bde361a19819ddc4089739ade06bab397172e9
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2024-01-10 14:34:50 -0800

    add logging utils + API

---
+++ frontend/packages/data-portal/app/types/logging.ts
@@ -0,0 +1,29 @@
+export enum LogLevel {
+  Log = 'log',
+  Debug = 'debug',
+  Error = 'error',
+  Info = 'info',
+  Warn = 'warn',
+  Trace = 'trace',
+}
+
+export interface LogEntry {
+  level: LogLevel
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  messages: any[]
+}
+
+export interface LogApiRequestBody {
+  logs: LogEntry[]
+}
+
+export interface LogApiErrorResponse {
+  status: 'error'
+  error: string
+}
+
+export interface LogApiSuccessResponse {
+  status: 'ok'
+}
+
+export type LogApiResponse = LogApiSuccessResponse | LogApiErrorResponse
---
+++ frontend/packages/data-portal/app/utils/logging.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+import { LogApiRequestBody, LogApiResponse, LogEntry } from 'app/types/logging'
+
+let LOG_QUEUE: LogEntry[] = []
+let logQueueTimeoutId: number | null = null
+
+export function sendLogs(logs: LogEntry[]) {
+  LOG_QUEUE.push(...logs)
+
+  if (!logQueueTimeoutId) {
+    logQueueTimeoutId = window.setTimeout(async () => {
+      try {
+        const response = await axios.post('/api/logs', {
+          logs: LOG_QUEUE,
+        } as LogApiRequestBody)
+        const data = response.data as LogApiResponse
+
+        if (data.status !== 'ok') {
+          throw new Error(data.error)
+        }
+      } catch (err) {
+        const error = err instanceof Error ? err.message : String(err)
+        // eslint-disable-next-line no-console
+        console.error(error)
+      } finally {
+        LOG_QUEUE = []
+      }
+    }, 500)
+  }
+}
