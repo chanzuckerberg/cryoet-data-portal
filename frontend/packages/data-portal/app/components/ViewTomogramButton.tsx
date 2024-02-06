05d8036dfccecc1489bc637b58141aea762f0ff4
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2024-02-06 13:07:56 -0800

    create view tomogram button component with tracking

---
+++ frontend/packages/data-portal/app/components/ViewTomogramButton.tsx
@@ -0,0 +1,54 @@
+import { Button, ButtonProps } from '@czi-sds/components'
+
+import { useI18n } from 'app/hooks/useI18n'
+import { EventPayloads, Events, usePlausible } from 'app/hooks/usePlausible'
+
+import { Link } from './Link'
+
+export function ViewTomogramButton({
+  buttonProps,
+  event,
+  neuroglancerConfig,
+}: {
+  buttonProps: Partial<ButtonProps>
+  event: EventPayloads[Events.ViewTomogram]
+  neuroglancerConfig: string | null | undefined
+}) {
+  const plausible = usePlausible()
+  const { t } = useI18n()
+
+  if (!neuroglancerConfig) {
+    return null
+  }
+
+  function trackViewTomogram() {
+    plausible(Events.ViewTomogram, event)
+  }
+
+  return (
+    // We need to disable this rule because we need the div to capture
+    // bubbled click events from the link button below. This is because
+    // Plausible automatically adds event listeners to every link on the
+    // page to track outbound links, so we can't attach a click listener
+    // to the link directly because Plausible will overwrite it.
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div
+      onClick={trackViewTomogram}
+      onKeyDown={({ key }) => {
+        if (key === 'Enter') {
+          trackViewTomogram()
+        }
+      }}
+    >
+      <Button
+        to={`https://neuroglancer-demo.appspot.com/#!${encodeURIComponent(
+          neuroglancerConfig,
+        )}`}
+        component={Link}
+        {...buttonProps}
+      >
+        <span>{t('viewTomogram')}</span>
+      </Button>
+    </div>
+  )
+}
