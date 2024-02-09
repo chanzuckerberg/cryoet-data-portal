fa6473f3f206906b4ca2efbdf54e3d433b22b65a
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2024-02-09 13:15:36 -0800

    remove redundant table count test

--- frontend/packages/data-portal/e2e/filters/utils.ts
+++ frontend/packages/data-portal/e2e/filters/utils.ts
@@ -4,14 +4,15 @@ import { expect, Page } from '@playwright/test'
 
 import { GetDatasetsDataQuery } from 'app/__generated__/graphql'
 
-const TABLE_COUNT_REGEX = /^([0-9]+) of ([0-9]+) Datasets$/
-
 async function waitForTableCountChange(
   page: Page,
   expectedFilterCount: number,
+  expectedTotalCount: number,
 ) {
   await page
-    .getByText(new RegExp(`^${expectedFilterCount} of [0-9]+ Datasets$`))
+    .getByText(
+      new RegExp(`^${expectedFilterCount} of ${expectedTotalCount} Datasets$`),
+    )
     .waitFor()
 }
 
@@ -21,8 +22,12 @@ async function waitForTableCountChange(
  * additional data is tested in this function to keep tests fast and focused on
  * testing the functionality of the filter.
  */
-export function getDatasetTableFilterValidator(data: GetDatasetsDataQuery) {
-  const datasetIdSet = new Set(data.datasets.map((dataset) => dataset.id))
+export function getDatasetTableFilterValidator(
+  expectedData: GetDatasetsDataQuery,
+) {
+  const datasetIdSet = new Set(
+    expectedData.datasets.map((dataset) => dataset.id),
+  )
 
   return async (page: Page) => {
     const datasetIds = await Promise.all(
@@ -52,23 +57,12 @@ export async function validateTable({
   page: Page
   validateRows(page: Page): Promise<void>
 }) {
-  const filterCount =
+  const expectedFilterCount =
     browseDatasetsData?.filtered_datasets_aggregate.aggregate?.count ?? 0
 
-  const totalCount =
+  const expectedTotalCount =
     browseDatasetsData?.datasets_aggregate.aggregate?.count ?? 0
 
-  await waitForTableCountChange(page, filterCount)
-
-  const tableCountText = await page.getByText(TABLE_COUNT_REGEX).textContent()
-
-  const match = TABLE_COUNT_REGEX.exec(tableCountText ?? '')
-
-  const actualFilterCount = +(match?.at(1) ?? '0')
-  const actualTotalCount = +(match?.at(2) ?? '0')
-
-  expect(actualFilterCount, 'Check filtered count').toBe(filterCount)
-  expect(actualTotalCount, 'Check total count').toBe(totalCount)
-
+  await waitForTableCountChange(page, expectedFilterCount, expectedTotalCount)
   await validateRows(page)
 }
