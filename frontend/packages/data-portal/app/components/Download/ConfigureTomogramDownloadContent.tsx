6b2a2da1ad6b1aee5d50b685d23840477461af2c
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2024-03-19 18:07:06 -0700

    add file format dropdown

--- frontend/packages/data-portal/app/components/Download/ConfigureTomogramDownloadContent.tsx
+++ frontend/packages/data-portal/app/components/Download/ConfigureTomogramDownloadContent.tsx
@@ -19,6 +19,7 @@ import { cns } from 'app/utils/cns'
 
 import { CopyBox } from '../CopyBox'
 import { Select, SelectOption } from '../Select'
+import { FileFormatDropdown } from './FileFormatDropdown'
 
 function Radio({
   children,
@@ -71,6 +72,8 @@ function Radio({
     </>,
   )
 }
+
+const TOMOGRAM_FILE_FORMATS = ['mrc', 'zarr']
 
 export function ConfigureTomogramDownloadContent() {
   const { t } = useI18n()
@@ -142,37 +145,44 @@ export function ConfigureTomogramDownloadContent() {
           description={t('selectASpecificTomogram')}
           onClick={setTomogramConfigWithInitialValues}
         >
-          <div className="flex items-center gap-sds-l pt-sds-m">
-            <Select
-              activeKey={
-                tomogramSampling
-                  ? t('unitAngstrom', { value: tomogramSampling })
-                  : null
-              }
-              className="flex-grow"
-              label={
-                activeTomogram
-                  ? `${t('unitAngstrom', {
-                      value: activeTomogram.voxel_spacing,
-                    })}`
-                  : '--'
-              }
-              onChange={(value) =>
-                setTomogramSampling(value ? value.replace('Å', '') : null)
-              }
-              options={tomogramSamplingOptions}
-              title={t('tomogramSampling')}
-            />
+          <div className="flex flex-col gap-sds-l">
+            <div className="flex items-center gap-sds-l pt-sds-m">
+              <Select
+                activeKey={
+                  tomogramSampling
+                    ? t('unitAngstrom', { value: tomogramSampling })
+                    : null
+                }
+                className="flex-grow"
+                label={
+                  activeTomogram
+                    ? `${t('unitAngstrom', {
+                        value: activeTomogram.voxel_spacing,
+                      })}`
+                    : '--'
+                }
+                onChange={(value) =>
+                  setTomogramSampling(value ? value.replace('Å', '') : null)
+                }
+                options={tomogramSamplingOptions}
+                title={t('tomogramSampling')}
+              />
 
-            <Select
-              activeKey={tomogramProcessing}
-              className="flex-grow"
-              label={tomogramProcessing ?? '--'}
-              onChange={setTomogramProcessing}
-              options={tomogramProcessingOptions}
-              showActiveValue={false}
-              showDetails={false}
-              title={t('tomogramProcessing')}
+              <Select
+                activeKey={tomogramProcessing}
+                className="flex-grow"
+                label={tomogramProcessing ?? '--'}
+                onChange={setTomogramProcessing}
+                options={tomogramProcessingOptions}
+                showActiveValue={false}
+                showDetails={false}
+                title={t('tomogramProcessing')}
+              />
+            </div>
+
+            <FileFormatDropdown
+              className="max-w-[228px]"
+              fileFormats={TOMOGRAM_FILE_FORMATS}
             />
           </div>
         </Radio>
