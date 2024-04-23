43064ba5ec1461a25b0b7a86e86f0d7e18e5342a
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2024-04-23 12:13:54 -0700

    feat: shift dataset photo (#613)
    #593
    
    - Shifts the single dataset photo to the left to be consistent with the
    single run page
    - Adds bottom border to the page header component to be reused on single
    dataset and run page
    
    ## Demos
    
    https://dev-nav-changes.cryoet.dev.si.czi.technology/
    
    <img width="1728" alt="image"
    src="https://github.com/chanzuckerberg/cryoet-data-portal/assets/2176050/7f7ff8be-a582-49d4-a0cc-41b7337b4f06">
    
    <img width="1728" alt="image"
    src="https://github.com/chanzuckerberg/cryoet-data-portal/assets/2176050/f94c0a7f-86d8-4f6a-8853-db1f9ab961de">

--- frontend/packages/data-portal/app/components/PageHeader.tsx
+++ frontend/packages/data-portal/app/components/PageHeader.tsx
@@ -37,140 +37,146 @@ export function PageHeader({
 
   return (
     <div className="flex flex-auto justify-center">
-      <header
-        className={cns(
-          'flex flex-col items-center',
-          'w-full min-h-[48px] max-w-content',
-        )}
-      >
-        <div className="flex flex-col justify-start w-full">
+      <header className="flex flex-col items-center w-full min-h-[48px]">
+        <div className="flex flex-col justify-start w-full pb-sds-xl">
           <div
             className={cns(
-              // create grid with fit content 1st row / 2nd col
-              'grid grid-cols-[1fr_auto] grid-rows-[1fr_auto]',
-              'w-full',
-              'justify-between',
-              'gap-x-sds-xxl',
-              'px-sds-xl pt-sds-l pb-sds-xxl',
+              'flex justify-center',
+              'border-b-[3px] border-[#d9d9d9]',
             )}
           >
-            {/* back button */}
-            {previousUrl && (
-              <div className="flex items-center">
-                <Link
-                  className="flex items-center gap-sds-xxs"
-                  to={previousUrl}
+            <div
+              className={cns(
+                // create grid with fit content 1st row / 2nd col
+                'grid grid-cols-[1fr_auto] grid-rows-[1fr_auto]',
+                'w-full max-w-content',
+                'justify-between',
+                'gap-x-sds-xxl',
+                'px-sds-xl pt-sds-l pb-sds-xxs',
+              )}
+            >
+              {/* back button */}
+              {previousUrl && (
+                <div className="flex items-center">
+                  <Link
+                    className="flex items-center gap-sds-xxs"
+                    to={previousUrl}
+                  >
+                    <Icon
+                      sdsIcon="chevronLeft"
+                      sdsSize="xs"
+                      sdsType="iconButton"
+                      className="!w-[10px] !h-[10px] !fill-sds-primary-400"
+                    />
+                    <span className="text-sds-primary-400 font-semibold text-sds-header-s leading-sds-header-s">
+                      {backToResultsLabel ?? t('backToResults')}
+                    </span>
+                  </Link>
+                </div>
+              )}
+
+              <div className="col-start-1 row-start-2 flex flex-col gap-sds-xxs">
+                <h1
+                  className={cns(
+                    'font-semibold',
+                    'text-sds-header-xxl leading-sds-header-xxl',
+                    'max-w-[1000px]',
+                  )}
                 >
-                  <Icon
-                    sdsIcon="chevronLeft"
-                    sdsSize="xs"
-                    sdsType="iconButton"
-                    className="!w-[10px] !h-[10px] !fill-sds-primary-400"
-                  />
-                  <span className="text-sds-primary-400 font-semibold text-sds-header-s leading-sds-header-s">
-                    {backToResultsLabel ?? t('backToResults')}
-                  </span>
-                </Link>
-              </div>
-            )}
+                  {title}
+                </h1>
 
-            <div className="col-start-1 row-start-2 flex flex-col gap-sds-xxs">
-              <h1
-                className={cns(
-                  'font-semibold',
-                  'text-sds-header-xxl leading-sds-header-xxl',
-                  'max-w-[1000px]',
-                )}
-              >
-                {title}
-              </h1>
-
-              {/* metadata */}
-              {metadata.length > 0 && (
-                <ul className="list-none flex gap-sds-l">
-                  {metadata.map(({ key, value, uppercase }) => (
-                    <li
-                      className="flex flex-row items-center justify-left gap-sds-xxs text-sds-gray-500"
-                      key={key + value}
-                    >
-                      <span
-                        className={cns(
-                          'font-semibold text-sds-caps-xxs leading-sds-caps-xxs tracking-sds-caps',
-                          uppercase && 'uppercase',
-                        )}
+                {/* metadata */}
+                {metadata.length > 0 && (
+                  <ul className="list-none flex gap-sds-l">
+                    {metadata.map(({ key, value, uppercase }) => (
+                      <li
+                        className="flex flex-row items-center justify-left gap-sds-xxs text-sds-gray-500"
+                        key={key + value}
                       >
-                        {key}:
-                      </span>
+                        <span
+                          className={cns(
+                            'font-semibold text-sds-caps-xxs leading-sds-caps-xxs tracking-sds-caps',
+                            uppercase && 'uppercase',
+                          )}
+                        >
+                          {key}:
+                        </span>
 
-                      <span className="text-sds-body-s leading-sds-body-s">
-                        {value}
-                      </span>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-
-            {/* dates */}
-            {(releaseDate || lastModifiedDate) && (
-              <div
-                className={cns(
-                  'row-start-1 col-start-2',
-                  'flex items-center justify-end gap-sds-xs',
-                  'text-xs text-sds-gray-600',
-                  'my-sds-l',
-                )}
-              >
-                {releaseDate && (
-                  <p>
-                    {t('releaseDate')}: {releaseDate}
-                  </p>
-                )}
-
-                <div className="h-3 w-px bg-sds-gray-400" />
-
-                {lastModifiedDate && (
-                  <p>
-                    {t('lastModified')}: {lastModifiedDate}
-                  </p>
+                        <span className="text-sds-body-s leading-sds-body-s">
+                          {value}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
                 )}
               </div>
-            )}
 
-            {/* actions */}
-            <div className="flex flex-row row-start-2 col-start-2 gap-sds-m justify-between min-w-[315px]">
-              {actions}
-
-              {onMoreInfoClick && (
-                <Button
-                  startIcon={
-                    <Icon sdsIcon="infoCircle" sdsType="button" sdsSize="l" />
-                  }
-                  sdsType="secondary"
-                  sdsStyle="rounded"
-                  onClick={onMoreInfoClick}
+              {/* dates */}
+              {(releaseDate || lastModifiedDate) && (
+                <div
+                  className={cns(
+                    'row-start-1 col-start-2',
+                    'flex items-center justify-end gap-sds-xs',
+                    'text-xs text-sds-gray-600',
+                    'my-sds-l',
+                  )}
                 >
-                  {t('moreInfo')}
-                </Button>
+                  {releaseDate && (
+                    <p>
+                      {t('releaseDate')}: {releaseDate}
+                    </p>
+                  )}
+
+                  <div className="h-3 w-px bg-sds-gray-400" />
+
+                  {lastModifiedDate && (
+                    <p>
+                      {t('lastModified')}: {lastModifiedDate}
+                    </p>
+                  )}
+                </div>
               )}
+
+              {/* actions */}
+              <div className="flex flex-row row-start-2 col-start-2 gap-sds-m justify-between min-w-[315px]">
+                {actions}
+
+                {onMoreInfoClick && (
+                  <Button
+                    startIcon={
+                      <Icon sdsIcon="infoCircle" sdsType="button" sdsSize="l" />
+                    }
+                    sdsType="secondary"
+                    sdsStyle="rounded"
+                    onClick={onMoreInfoClick}
+                  >
+                    {t('moreInfo')}
+                  </Button>
+                )}
+              </div>
             </div>
           </div>
 
-          {renderHeader?.({
-            moreInfo: onMoreInfoClick && (
-              <div className="flex w-full">
-                <Button
-                  className="flex items-center gap-sds-xxs"
-                  sdsType="primary"
-                  sdsStyle="minimal"
-                  onClick={onMoreInfoClick}
-                >
-                  <Icon sdsIcon="infoCircle" sdsSize="s" sdsType="button" />
-                  <span>{t('moreInfo')}</span>
-                </Button>
-              </div>
-            ),
-          })}
+          <div className="flex justify-center">
+            <div className="flex flex-auto max-w-content">
+              {renderHeader?.({
+                moreInfo: onMoreInfoClick && (
+                  <div className="flex w-full">
+                    <Button
+                      className="flex items-center gap-sds-xxs"
+                      sdsType="primary"
+                      sdsStyle="minimal"
+                      onClick={onMoreInfoClick}
+                    >
+                      <Icon sdsIcon="infoCircle" sdsSize="s" sdsType="button" />
+                      <span>{t('moreInfo')}</span>
+                    </Button>
+                  </div>
+                ),
+              })}
+            </div>
+          </div>
         </div>
       </header>
     </div>
