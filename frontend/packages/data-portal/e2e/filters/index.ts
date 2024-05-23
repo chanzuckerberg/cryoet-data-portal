a17426424f2e5b3971db627b2c4b5448bcdf4244
Jeremy <jeremyasuncion808@gmail.com>
2024-05-23 11:27:50 -0700

    remove imports

--- frontend/packages/data-portal/e2e/filters/index.ts
+++ frontend/packages/data-portal/e2e/filters/index.ts
@@ -1,6 +1,4 @@
-export * from './testAvailableFilesFilter'
 export * from './testGroundTruthAnnotationFilter'
 export * from './testMultiInputFilter'
 export * from './testMultiSelectFilter'
-export * from './testOrganismNameFilter'
 export * from './testSingleSelectFilter'
