0bca989556a208c32969cfb00aab912b49a3c86c
Kira Evans <contact@kne42.me>
2024-05-29 12:52:25 -0700

    put index page CTA behind feature flag

--- frontend/packages/data-portal/app/components/Index/IndexCTA.tsx
+++ frontend/packages/data-portal/app/components/Index/IndexCTA.tsx
@@ -3,6 +3,8 @@ import { Button } from '@czi-sds/components'
 import { I18n } from 'app/components/I18n'
 import { Link } from 'app/components/Link'
 import { useI18n } from 'app/hooks/useI18n'
+import { cns } from 'app/utils/cns'
+import { useFeatureFlag } from 'app/utils/featureFlags'
 
 function CTA({
   title,
@@ -36,13 +38,21 @@ function CTA({
 
 export function IndexCTA() {
   const { t } = useI18n()
+  const showMlChallengePage = useFeatureFlag('mlChallenge')
 
   return (
     <div className="py-sds-xxl flex flex-col gap-sds-xl relative after:h-full after:w-[200vw] after:bg-sds-primary-100 after:absolute after:top-0 after:-translate-x-1/2 after:-z-10">
       <h3 className="font-sds-semibold font-semibold text-sds-header-xl leading-sds-header-xl">
         <I18n i18nKey="helpUsAchieveThisVision" />
       </h3>
-      <div className="w-full grid grid-cols-[1fr_auto_1fr_auto_1fr] grid-rows-[1fr_auto] grid-flow-col gap-y-sds-xl gap-x-sds-xxl">
+      <div
+        className={cns(
+          'w-full grid grid-rows-[1fr_auto] grid-flow-col gap-y-sds-xl gap-x-sds-xxl',
+          showMlChallengePage
+            ? 'grid-cols-[1fr_auto_1fr_auto_1fr]'
+            : 'grid-cols-[1fr_auto_1fr]',
+        )}
+      >
         <CTA
           title={t('viewAndDownloadDatasets')}
           text={t('viewDatasetsCta')}
@@ -56,13 +66,17 @@ export function IndexCTA() {
           buttonText={t('tellUsMore')}
           url={t('urlDataContributionForm')}
         />
-        <div className="bg-sds-gray-200 w-sds-xxxs row-span-2" />
-        <CTA
-          title={t('participateInOurCompetition')}
-          text={t('participateInOurCompetitionCTA')}
-          buttonText={t('learnMore')}
-          url="/competition"
-        />
+        {showMlChallengePage && (
+          <>
+            <div className="bg-sds-gray-200 w-sds-xxxs row-span-2" />
+            <CTA
+              title={t('participateInOurCompetition')}
+              text={t('participateInOurCompetitionCTA')}
+              buttonText={t('learnMore')}
+              url="/competition"
+            />
+          </>
+        )}
       </div>
     </div>
   )
