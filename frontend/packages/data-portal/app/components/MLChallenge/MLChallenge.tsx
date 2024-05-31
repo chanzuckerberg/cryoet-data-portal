47e9b457874cf531b93fbe69bb933537760efd66
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2024-05-30 18:07:54 -0700

    feat: ML Challenge Banners (#755)
    #711
    
    Adds top and bottom banner to the ML challenge page
    
    ## Demos
    
    Ignore the red lines in the screenshot, that's part of some demo content
    I was using to test the header / footer scrolling
    
    <img width="1728" alt="image"
    src="https://github.com/chanzuckerberg/cryoet-data-portal/assets/2176050/9eadf061-d459-4548-8cb1-69e40a639d9c">
    
    <img width="1728" alt="image"
    src="https://github.com/chanzuckerberg/cryoet-data-portal/assets/2176050/a5b6f469-9528-4e41-9035-864499353c83">
    
    ---------
    
    Co-authored-by: Kira Evans <contact@kne42.me>

--- frontend/packages/data-portal/app/components/MLChallenge/MLChallenge.tsx
+++ frontend/packages/data-portal/app/components/MLChallenge/MLChallenge.tsx
@@ -1,13 +1,16 @@
 import { MainContent } from './MainContent'
+import { MLChallengeHeader } from './MLChallengeHeader'
 
 export function MLChallenge() {
   return (
-    <div className="flex flex-col px-sds-xl w-[100vw] overflow-x-clip">
-      {/* TODO: Header */}
-      <div className="flex flex-row w-full justify-between">
+    <div className="flex flex-col w-[100vw] overflow-x-clip">
+      <MLChallengeHeader />
+
+      <div className="flex flex-row w-full justify-between px-sds-xl">
         {/* Filler Component for Spacing */}
         <div className="basis-0 flex-1 max-w-[160px]" />
         <MainContent />
+
         <div className="mt-sds-xl ml-sds-xxl w-[160px] h-[260px] bg-sds-gray-500 flex-shrink-0 hidden screen-760:block">
           {/* TODO: NavBar */}
         </div>
---
+++ frontend/packages/data-portal/app/components/MLChallenge/MLChallengeHeader.tsx
@@ -0,0 +1,34 @@
+import { useI18n } from 'app/hooks/useI18n'
+
+function ChallengeInfo({ title, content }: { title: string; content: string }) {
+  return (
+    <div className="font-semibold">
+      <p className="text-sds-header-xxxs leading-sds-header-xxxs text-sds-gray-600">
+        {title}
+      </p>
+      <p className="text-sds-body-l leading-sds-body-l">{content}</p>
+    </div>
+  )
+}
+
+export function MLChallengeHeader() {
+  const { t } = useI18n()
+
+  return (
+    <div className="bg-sds-primary-200 flex flex-col justify-center py-sds-xxl text-center">
+      <h1 className="text-sds-header-xxl leading-sds-header-xxl font-semibold">
+        {t('cryoetDataAnnotationMLComp')}
+      </h1>
+
+      <h2 className="text-sds-header-m leading-sds-header-m mt-sds-xs">
+        {t('developAMLModel')}
+      </h2>
+
+      <div className="flex items-center mt-sds-xl justify-center">
+        <ChallengeInfo title={t('starts')} content={t('fall2024')} />
+        <div className="h-full w-px bg-sds-gray-400 mx-sds-xl" />
+        <ChallengeInfo title={t('prizes')} content={t('prizeTotal')} />
+      </div>
+    </div>
+  )
+}
