0f6c3a9fdaa894275cf269ae757e74b7085e5b18
Jeremy <jeremyasuncion808@gmail.com>
2024-06-07 17:18:50 -0700

    use e2e config from file and merge with env

--- frontend/packages/data-portal/e2e/constants.ts
+++ frontend/packages/data-portal/e2e/constants.ts
@@ -1,13 +1,19 @@
 import { readFileSync } from 'node:fs'
 
-import type config from 'e2e/config.example.json'
-import type translationsType from 'public/locales/en/translation.json'
+import baseConfig from 'e2e/config.json' assert { type: 'json' }
+import { merge } from 'lodash-es'
+import type Translations from 'public/locales/en/translation.json'
+
+type E2EConfig = typeof baseConfig
 
 export const translations = JSON.parse(
   readFileSync('public/locales/en/translation.json', 'utf-8'),
-) as typeof translationsType
+) as typeof Translations
 
-export const E2E_CONFIG = JSON.parse(process.env.E2E_CONFIG) as typeof config
+export const E2E_CONFIG = merge(
+  baseConfig,
+  JSON.parse(process.env.E2E_CONFIG ?? '{}'),
+) as E2EConfig
 
 export const BROWSE_DATASETS_URL = `${E2E_CONFIG.url}/browse-data/datasets`
 export const SINGLE_DATASET_URL = `${E2E_CONFIG.url}/datasets/${E2E_CONFIG.datasetId}`
