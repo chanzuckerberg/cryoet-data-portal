f6f88d096a7325b1a1ed72260dda9d7eddfe81ef
Jeremy <jeremyasuncion808@gmail.com>
2024-06-11 10:44:48 -0700

    update code snippets

--- frontend/packages/data-portal/app/components/Download/APIDownloadTab.tsx
+++ frontend/packages/data-portal/app/components/Download/APIDownloadTab.tsx
@@ -1,4 +1,5 @@
 import { Callout } from '@czi-sds/components'
+import dedent from 'dedent'
 import { useMemo } from 'react'
 import { match, P } from 'ts-pattern'
 
@@ -29,14 +30,35 @@ export function APIDownloadTab() {
       match({ fileFormat, type, downloadConfig })
         .with({ type: 'dataset' }, () => ({
           label: t('datasetId'),
-          content: datasetId,
+          content: dedent`
+            from cryoet_data_portal import Client, Dataset
+
+            client = Client()
+
+            dataset = Dataset.get_by_id(client, ${datasetId})
+            dataset.download_everything('${datasetId}')
+          `,
           logType: 'dataset-id',
         }))
         .with(
           { type: 'runs', downloadConfig: DownloadConfig.AllAnnotations },
           () => ({
             label: t('voxelSpacingId'),
-            content: tomogramVoxelId,
+            content: dedent`
+              from cryoet_data_portal import (
+                Client,
+                TomogramVoxelSpacing,
+              )
+
+              client = Client()
+              tomogram_voxel_spacing = TomogramVoxelSpacing.get_by_id(
+                client,
+                ${tomogramVoxelId},
+              )
+
+              for annotation in tomogram_voxel_spacing.annotations:
+                annotation.download()
+            `,
             logType: 'voxel-spacing-id',
           }),
         )
@@ -48,39 +70,34 @@ export function APIDownloadTab() {
           },
           () => ({
             label: t('copyApiCodeSnippet'),
-            content: (
-              <>
-                from cryoet_data_portal import Client, Tomogram
-                <br />
-                client = Client()
-                <br />
-                tomogram = Tomogram.get_by_id(client, {tomogramId})
-                <br />
-                tomogram.{downloadFunction}()
-              </>
-            ),
+            content: dedent`
+              from cryoet_data_portal import Client, Tomogram
+
+              client = Client()
+
+              tomogram = Tomogram.get_by_id(client, ${tomogramId})
+              tomogram.${downloadFunction}()
+            `,
             logType: 'tomogram-code-snippet',
           }),
         )
         .with({ type: 'annotation', fileFormat: P.string }, () => ({
           label: t('copyApiCodeSnippet'),
-          content: (
-            <>
-              from cryoet_data_portal import Client, Annotation
-              <br />
-              client = Client()
-              <br />
-              annotation = Annotation.get_by_id(client, {annotationId})
-              <br />
-              annotation.download(format=&quot;{fileFormat}&quot;)
-            </>
-          ),
+          content: dedent`
+            from cryoet_data_portal import Client, Annotation
+
+
+            client = Client()
+
+            annotation = Annotation.get_by_id(client, ${annotationId})
+            annotation.download(format='${fileFormat}')
+          `,
           logType: 'annotation-code-snippet',
         }))
         .otherwise(() => ({
           // no idea why this is throwing an error
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          label: t('tomogramId'),
+          // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+          label: t('tomogramId') as string,
           content: tomogramId,
           logType: 'tomogram-id',
         })),
@@ -117,6 +134,7 @@ export function APIDownloadTab() {
 
       <CopyBox
         className="mt-sds-l"
+        codeClassName="whitespace-pre"
         content={content}
         title={label}
         onCopy={() => logPlausibleCopyEvent(logType, String(content))}
