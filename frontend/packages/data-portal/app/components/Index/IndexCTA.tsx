982aba9008eb68296063d1163e23748d5d72d3be
Jeremy <jeremyasuncion808@gmail.com>
2024-06-13 16:07:13 -0700

    clean up feature flags

--- frontend/packages/data-portal/app/components/Index/IndexCTA.tsx
+++ frontend/packages/data-portal/app/components/Index/IndexCTA.tsx
@@ -4,7 +4,6 @@ import { I18n } from 'app/components/I18n'
 import { Link } from 'app/components/Link'
 import { useI18n } from 'app/hooks/useI18n'
 import { cns } from 'app/utils/cns'
-import { useFeatureFlag } from 'app/utils/featureFlags'
 
 function CTA({
   title,
@@ -38,7 +37,6 @@ function CTA({
 
 export function IndexCTA() {
   const { t } = useI18n()
-  const showMlChallengePage = useFeatureFlag('mlChallenge')
 
   return (
     <div className="py-sds-xxl flex flex-col gap-sds-xl relative after:h-full after:w-[200vw] after:bg-sds-primary-100 after:absolute after:top-0 after:-translate-x-1/2 after:-z-10">
@@ -48,9 +46,7 @@ export function IndexCTA() {
       <div
         className={cns(
           'w-full grid grid-rows-[1fr_auto] grid-flow-col gap-y-sds-xl gap-x-sds-xxl',
-          showMlChallengePage
-            ? 'grid-cols-[1fr_auto_1fr_auto_1fr]'
-            : 'grid-cols-[1fr_auto_1fr]',
+          'grid-cols-[1fr_auto_1fr_auto_1fr]',
         )}
       >
         <CTA
