e531f84830092effa01f19ec0d400ada6d578417
Bryan Chu <bchu@chanzuckerberg.com>
2024-08-05 12:15:16 -0700

    fix query

--- frontend/packages/data-portal/app/components/Run/RunMetadataDrawer.tsx
+++ frontend/packages/data-portal/app/components/Run/RunMetadataDrawer.tsx
@@ -6,7 +6,7 @@ import { useRunById } from 'app/hooks/useRunById'
 import { i18n } from 'app/i18n'
 
 import { RunTiltSeriesTable } from './RunTiltSeriesTable'
-import { TomogramsMetadataSection } from './TomogramsTable'
+import { TomogramsMetadataSection } from './TomogramsMetadataSection'
 
 export function RunMetadataDrawer() {
   const { run } = useRunById()
