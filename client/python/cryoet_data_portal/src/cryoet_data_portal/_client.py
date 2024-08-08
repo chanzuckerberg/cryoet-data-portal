455fe701c9f450a92f15adaaccb0167b3cba21a9
Andy Sweet <andrew.d.sweet@gmail.com>
2024-08-08 14:10:26 -0700

    Add support for depositions

--- client/python/cryoet_data_portal/src/cryoet_data_portal/_client.py
+++ client/python/cryoet_data_portal/src/cryoet_data_portal/_client.py
@@ -8,7 +8,8 @@ from gql.transport.requests import RequestsHTTPTransport
 
 from cryoet_data_portal._constants import USER_AGENT
 
-DEFAULT_URL = "https://graphql.cryoetdataportal.cziscience.com/v1/graphql"
+# TODO: change to production when live.
+DEFAULT_URL = "https://graphql-cryoet-api.cryoet.staging.si.czi.technology/v1/graphql"
 
 
 class Client:
