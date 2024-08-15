7bbb4b5310f96504765db24c2f20330d6517fa3c
Bryan Chu <bchu@chanzuckerberg.com>
2024-08-15 12:15:30 -0700

    test

--- frontend/packages/data-portal/e2e/pageObjects/singleRunPage.ts
+++ frontend/packages/data-portal/e2e/pageObjects/singleRunPage.ts
@@ -1,16 +1,45 @@
-import { Locator } from '@playwright/test'
-import { SINGLE_RUN_URL } from 'e2e/constants'
+import {
+  ApolloClient,
+  ApolloQueryResult,
+  NormalizedCacheObject,
+} from '@apollo/client'
+import { Locator, Page } from '@playwright/test'
+import { E2E_CONFIG, SINGLE_RUN_URL } from 'e2e/constants'
+
+import { GetRunByIdQuery } from 'app/__generated__/graphql'
+import { getRunById } from 'app/graphql/getRunById.server'
 
 import { BasePage } from './basePage'
 
 /** /runs/$id */
 export class SingleRunPage extends BasePage {
+  constructor(
+    playwrightPage: Page,
+    private readonly client: ApolloClient<NormalizedCacheObject>,
+  ) {
+    super(playwrightPage)
+  }
+
   goToPage(): Promise<void> {
     return this.goTo(SINGLE_RUN_URL)
   }
 
+  loadData(): Promise<ApolloQueryResult<GetRunByIdQuery>> {
+    return getRunById({
+      client: this.client,
+      id: Number(E2E_CONFIG.runId),
+      annotationsPage: 1,
+    })
+  }
+
   getPrimaryViewTomogramButton(): Locator {
     return this.page.locator('a:has-text("View Tomogram")')
+  }
+
+  findProcessingMethodsCell(): Locator {
+    return this.page
+      .locator(`td:has-text("Tomogram Processing")`)
+      .locator('+ td')
   }
 
   findAnnotatedObjectsCell(): Locator {
