0e5decb76c9761980521111c1ea7848fdef15676
Jeremy <jeremyasuncion808@gmail.com>
2024-08-23 14:32:40 -0700

    deposition filter banner component

---
+++ frontend/packages/data-portal/app/components/DepositionFilterBanner.tsx
@@ -0,0 +1,43 @@
+import { Button, Callout } from '@czi-sds/components'
+
+import { I18n } from 'app/components/I18n'
+import { QueryParams } from 'app/constants/query'
+import { useQueryParam } from 'app/hooks/useQueryParam'
+import { I18nKeys } from 'app/types/i18n'
+
+interface Deposition {
+  id: number
+  title: string
+}
+
+export function DepositionFilterBanner({
+  deposition,
+  labelI18n,
+}: {
+  deposition: Deposition
+  labelI18n: I18nKeys
+}) {
+  const [, setDepositionId] = useQueryParam<string>(QueryParams.DepositionId)
+
+  return (
+    <Callout
+      className="!w-full !mx-sds-xl"
+      classes={{ message: 'w-full' }}
+      intent="info"
+    >
+      <div className="flex w-full items-center justify-between">
+        <p className="text-sds-body-xs leading-sds-body-xs">
+          <I18n i18nKey={labelI18n} values={deposition} />
+        </p>
+
+        <Button
+          onClick={() => setDepositionId(null)}
+          sdsStyle="minimal"
+          sdsType="secondary"
+        >
+          Remove Filter
+        </Button>
+      </div>
+    </Callout>
+  )
+}
