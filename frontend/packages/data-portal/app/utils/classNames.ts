570e10953eef4a7f9da5e7610d75661335efb639
Jeremy <jeremyasuncion808@gmail.com>
2024-08-23 14:32:41 -0700

    extract dash class names to util

---
+++ frontend/packages/data-portal/app/utils/classNames.ts
@@ -0,0 +1,5 @@
+export const DASHED_BORDERED_CLASSES =
+  'border-b border-dashed hover:border-solid border-current'
+
+export const DASHED_UNDERLINED_CLASSES =
+  'underline underline-offset-[3px] decoration-dashed hover:decoration-solid'
