3e67f8dd6731aebcd82965da815187c53dbb2ef5
Jeremy <jeremyasuncion808@gmail.com>
2024-08-30 22:36:38 -0700

    render method links

--- frontend/packages/data-portal/app/components/Run/AnnotationOveriewTable.tsx
+++ frontend/packages/data-portal/app/components/Run/AnnotationOveriewTable.tsx
@@ -4,6 +4,48 @@ import { AuthorList } from 'app/components/AuthorList'
 import { useI18n } from 'app/hooks/useI18n'
 import { useAnnotation } from 'app/state/annotation'
 
+import { MethodLink } from '../Deposition/MethodLinks'
+import {
+  generateMethodLinks,
+  MethodLinkVariantProps,
+} from '../Deposition/MethodLinks/common'
+import { Link } from '../Link'
+
+const METHOD_LINK_VARIANTS: MethodLinkVariantProps[] = [
+  {
+    variant: 'sourceCode',
+    url: 'https://www.example.com',
+    title: "My model's source code",
+  },
+  {
+    variant: 'sourceCode',
+    url: 'https://www.example.com',
+    title: 'Lorem-Ipsum3-D source code',
+  },
+  {
+    variant: 'modelWeights',
+    url: 'https://www.example.com',
+    title: 'Model Weights',
+  },
+  {
+    variant: 'website',
+    url: 'https://www.example.com',
+    title: 'Optional Custom Link Name',
+  },
+  {
+    variant: 'documentation',
+    url: 'https://www.example.com',
+    title: 'Optional Custom Link Name',
+  },
+  {
+    variant: 'other',
+    url: 'https://www.example.com',
+    title: 'Optional Custom Link Name',
+  },
+]
+
+const METHOD_LINKS = generateMethodLinks(METHOD_LINK_VARIANTS)
+
 export function AnnotationOverviewTable() {
   const { activeAnnotation: annotation } = useAnnotation()
   const { t } = useI18n()
@@ -38,6 +80,19 @@ export function AnnotationOverviewTable() {
           values: [annotation.annotation_publication ?? '--'],
         },
         {
+          label: t('depositionName'),
+          values: ['Deposition Name'],
+          renderValue: () => (
+            <Link className="text-sds-primary-400" to="/deposition/123">
+              Deposition Name
+            </Link>
+          ),
+        },
+        {
+          label: t('depositionId'),
+          values: ['CZCDP-12345'],
+        },
+        {
           label: t('depositionDate'),
           values: [annotation.deposition_date],
         },
@@ -60,6 +115,20 @@ export function AnnotationOverviewTable() {
         {
           label: t('annotationSoftware'),
           values: [annotation.annotation_software ?? '--'],
+        },
+        {
+          label: t('methodLinks'),
+          // No value required for this field, render only links component
+          values: [''],
+          renderValue: () => (
+            <ul>
+              {METHOD_LINKS.map((link) => (
+                <li key={`${link.url}_${link.i18nLabel}_${link.title}`}>
+                  <MethodLink {...link} />
+                </li>
+              ))}
+            </ul>
+          ),
         },
       ]}
     />
