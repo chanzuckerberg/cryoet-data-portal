05f7e4f84fbfaa41c92417e9c9b7f5d3f64b67fd
Bryan Chu <bchu@chanzuckerberg.com>
2024-09-03 13:24:53 -0700

    fix typo again

--- frontend/packages/data-portal/app/components/PageHeader.tsx
+++ frontend/packages/data-portal/app/components/PageHeader.tsx
@@ -28,7 +28,7 @@ export function PageHeader({
 
   return (
     <div className="flex flex-auto justify-center grow-0">
-      <header className="flex flex-col items-center w-full min-h-[48x]">
+      <header className="flex flex-col items-center w-full min-h-[48px]">
         <div className="flex flex-col justify-start w-full pb-sds-xl">
           <div
             className={cns(
