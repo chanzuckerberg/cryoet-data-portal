fadc2622cafd8290c2427c775c860f808796ab3b
Jeremy <jeremyasuncion808@gmail.com>
2024-09-13 22:55:06 -0700

    replace history state when updating filters

--- frontend/packages/data-portal/app/hooks/useFilter.ts
+++ frontend/packages/data-portal/app/hooks/useFilter.ts
@@ -126,46 +126,57 @@ export function useFilter() {
       ...getFilterState(searchParams),
 
       reset() {
-        setSearchParams((prev) => {
-          Object.values(QueryParams).forEach((param) => prev.delete(param))
-          prev.delete('page')
+        setSearchParams(
+          (prev) => {
+            Object.values(QueryParams).forEach((param) => prev.delete(param))
+            prev.delete('page')
 
-          return prev
-        })
+            return prev
+          },
+          { replace: true },
+        )
       },
 
       updateValue(param: QueryParams, value?: FilterValue) {
         logPlausibleEvent(param, value)
 
-        setSearchParams((prev) => {
-          prev.delete(param)
-          prev.delete('page')
+        setSearchParams(
+          (prev) => {
+            prev.delete(param)
+            prev.delete('page')
 
-          if (value) {
-            normalizeFilterValue(value).forEach((v) => prev.append(param, v))
-          }
+            if (value) {
+              normalizeFilterValue(value).forEach((v) => prev.append(param, v))
+            }
 
-          return prev
-        })
+            return prev
+          },
+          { replace: true },
+        )
       },
 
       updateValues(params: Partial<Record<QueryParams, FilterValue>>) {
         const entries = Object.entries(params) as [QueryParams, FilterValue][]
         entries.forEach(([param, value]) => logPlausibleEvent(param, value))
 
-        setSearchParams((prev) => {
-          prev.delete('page')
+        setSearchParams(
+          (prev) => {
+            prev.delete('page')
 
-          entries.forEach(([param, value]) => {
-            prev.delete(param)
+            entries.forEach(([param, value]) => {
+              prev.delete(param)
 
-            if (value) {
-              normalizeFilterValue(value).forEach((v) => prev.append(param, v))
-            }
-          })
+              if (value) {
+                normalizeFilterValue(value).forEach((v) =>
+                  prev.append(param, v),
+                )
+              }
+            })
 
-          return prev
-        })
+            return prev
+          },
+          { replace: true },
+        )
       },
     }),
 
