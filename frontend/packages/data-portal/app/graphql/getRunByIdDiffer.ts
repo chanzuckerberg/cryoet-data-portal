469b9b8eff0d7fe3d2de3e436d5f4f83262949e1
Bryan Chu <151689101+bchu1@users.noreply.github.com>
2024-09-19 15:26:57 -0700

    feat: Stop querying publications in V2 temporarily (#1160)
    The field's name is being changed.

--- frontend/packages/data-portal/app/graphql/getRunByIdDiffer.ts
+++ frontend/packages/data-portal/app/graphql/getRunByIdDiffer.ts
@@ -70,9 +70,12 @@ export function logIfHasDiff(
         id: run.dataset.id,
         lastModifiedDate: `${run.dataset.last_modified_date}T00:00:00+00:00`,
         organismName: run.dataset.organism_name!,
-        organismTaxid: Number(run.dataset.organism_taxid),
+        organismTaxid:
+          run.dataset.organism_taxid != null
+            ? Number(run.dataset.organism_taxid)
+            : run.dataset.organism_taxid,
         otherSetup: run.dataset.other_setup,
-        publications: run.dataset.dataset_publications,
+        // publications: run.dataset.dataset_publications,
         relatedDatabaseEntries: run.dataset.related_database_entries,
         releaseDate: `${run.dataset.release_date}T00:00:00+00:00`,
         s3Prefix: run.dataset.s3_prefix,
