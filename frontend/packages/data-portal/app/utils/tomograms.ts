e64f9b8d5ded6ad203787a507804d816dc15ae3a
Bryan Chu <bchu@chanzuckerberg.com>
2024-09-20 14:17:52 -0700

    lint

--- frontend/packages/data-portal/app/utils/tomograms.ts
+++ frontend/packages/data-portal/app/utils/tomograms.ts
@@ -1,14 +1,13 @@
 import { startCase } from 'lodash-es'
 
 import { Tomogram as DownloadTomogramType } from 'app/context/DownloadModal.context'
-import { Tomogram as TableTomogramType } from 'app/state/metadataDrawerTomogram'
 
 export function isFiducial(status: string | null | undefined) {
   return status === 'FIDUCIAL'
 }
 
 export function getTomogramName(
-  tomogram: DownloadTomogramType | TableTomogramType, // TODO(bchu): Replace with single Platformics type.
+  tomogram: DownloadTomogramType, // TODO(bchu): Replace with single Platformics type.
 ): string {
   return startCase(
     `${tomogram.id} ${tomogram.reconstruction_method} ${tomogram.processing}`,
