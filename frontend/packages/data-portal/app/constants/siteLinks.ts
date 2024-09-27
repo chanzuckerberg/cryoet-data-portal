f07f4abfd3d89c4edeb9dc306f0c368ca8cacadc
Melissa Weber Mendonça <melissawm@gmail.com>
2024-09-27 17:46:41 -0300

    docs: Fix link to FAQ in About & Help dropwdown

--- frontend/packages/data-portal/app/constants/siteLinks.ts
+++ frontend/packages/data-portal/app/constants/siteLinks.ts
@@ -2,7 +2,7 @@ export enum SiteLinks {
   HOME = '/',
   COMPETITION = '/competition',
   BROWSE_DATA_DATASETS = '/browse-data/datasets',
-  FAQ = '/faq',
+  FAQ = 'https://chanzuckerberg.github.io/cryoet-data-portal/cryoet_data_portal_docsite_faq.html',
   PRIVACY = '/privacy',
   DATA_SUBMISSION_POLICY = '/data-submission-policy',
 }
