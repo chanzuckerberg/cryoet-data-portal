8ad5563d955e3cfeecfe71e40de776312cd50104
Jessica Gadling <jgadling@chanzuckerberg.com>
2024-10-01 16:52:54 -0400

    Start updating ci for apiv2

--- client/python/cryoet_data_portal/tests/test_infra/seed_moto.sh
+++ client/python/cryoet_data_portal/tests/test_infra/seed_moto.sh
@@ -2,14 +2,15 @@
 
 # Script to seed moto server; runs outside the motoserver container for development
 
-aws="aws --endpoint-url=http://localhost:4000"
+aws="aws --endpoint-url=http://localhost:5566"
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 export AWS_REGION=us-west-2
 
-# Create dev bucket but don't error if it already exists
-test_file=test_file
-echo test_data > $test_file
+# Wipe out the bucket so we can start fresh.
+$aws s3api delete-bucket --bucket $bucket_1 2>/dev/null || /bin/true
+
+# Create dev bucket but don't error if it already exists (and it shouldn't exist)
 bucket_1=test-public-bucket
 $aws s3api head-bucket --bucket $bucket_1 2>/dev/null || $aws s3 mb s3://$bucket_1
 
