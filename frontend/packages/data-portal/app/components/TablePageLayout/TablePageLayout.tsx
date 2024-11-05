996d2d82bc5d3c46a135894b673b5118b0aa571b
Jeremy <jeremyasuncion808@gmail.com>
2024-11-05 10:03:07 -0800

    fix alignment + remove layout context

--- frontend/packages/data-portal/app/components/TablePageLayout/TablePageLayout.tsx
+++ frontend/packages/data-portal/app/components/TablePageLayout/TablePageLayout.tsx
@@ -1,13 +1,12 @@
 import { Pagination } from '@czi-sds/components'
 import { useSearchParams } from '@remix-run/react'
-import { ComponentType, ReactNode, useEffect, useMemo } from 'react'
+import { ComponentType, ReactNode, useEffect } from 'react'
 
 import { Tabs } from 'app/components/Tabs'
 import { TABLE_PAGE_LAYOUT_LOG_ID } from 'app/constants/error'
 import { MAX_PER_PAGE } from 'app/constants/pagination'
 import { QueryParams } from 'app/constants/query'
 import { TestIds } from 'app/constants/testIds'
-import { LayoutContext, LayoutContextValue } from 'app/context/Layout.context'
 import { cns } from 'app/utils/cns'
 
 import { ErrorBoundary } from '../ErrorBoundary'
@@ -141,93 +140,82 @@ function TablePageTabContent({
     })
   }
 
-  const contextValue = useMemo<LayoutContextValue>(
-    () => ({
-      hasFilters: !!filterPanel,
-    }),
-    [filterPanel],
-  )
-
   if (noTotalResults !== undefined && totalCount === 0) {
     return noTotalResults
   }
 
   return (
-    <LayoutContext.Provider value={contextValue}>
-      <div className="flex flex-auto">
-        {filterPanel && (
-          <div
-            className={cns(
-              'flex flex-col flex-shrink-0 w-[235px]',
-              'border-t border-r border-sds-color-primitive-gray-300',
-            )}
-          >
-            {filterPanel}
-          </div>
-        )}
-
+    <div className="flex flex-auto">
+      {filterPanel && (
         <div
           className={cns(
-            'flex flex-col flex-auto',
-            'pb-sds-xxl',
-            'border-t border-sds-color-primitive-gray-300',
-            'overflow-x-scroll max-w-full',
-            !banner && 'pt-sds-xl',
-
-            filterPanel
-              ? 'screen-2040:items-center'
-              : 'screen-1024:items-center',
+            'flex flex-col flex-shrink-0 w-[235px]',
+            'border-t border-r border-sds-color-primitive-gray-300',
           )}
         >
-          <div
-            className={cns(
-              'flex flex-col flex-auto w-full max-w-content',
+          {filterPanel}
+        </div>
+      )}
 
-              // Translate to the left by half the filter panel width to align with the header
-              filterPanel && 'screen-2040:translate-x-[-117.5px]',
+      <div
+        className={cns(
+          'flex flex-col flex-auto',
+          'pb-sds-xxl',
+          'border-t border-sds-color-primitive-gray-300',
+          'overflow-x-scroll max-w-full',
+          !banner && 'pt-sds-xl',
+
+          filterPanel ? 'screen-2040:items-center' : 'screen-1024:items-center',
+        )}
+      >
+        <div
+          className={cns(
+            'flex flex-col flex-auto w-full max-w-content',
+
+            // Translate to the left by half the filter panel width to align with the header
+            filterPanel && 'screen-2040:translate-x-[-117.5px]',
+          )}
+        >
+          {banner && <div className="flex px-sds-xl">{banner}</div>}
+
+          <Header
+            countLabel={countLabel}
+            description={description}
+            filteredCount={filteredCount}
+            learnMoreLink={learnMoreLink}
+            title={title}
+            totalCount={totalCount}
+          />
+
+          <ErrorBoundary logId={TABLE_PAGE_LAYOUT_LOG_ID}>
+            <div className="overflow-x-scroll">{table}</div>
+          </ErrorBoundary>
+
+          <div className="px-sds-xl">
+            {filteredCount === 0 && (
+              <div className="mt-[100px]">{noFilteredResults}</div>
             )}
-          >
-            {banner && <div className="flex px-sds-xl">{banner}</div>}
 
-            <Header
-              countLabel={countLabel}
-              description={description}
-              filteredCount={filteredCount}
-              learnMoreLink={learnMoreLink}
-              title={title}
-              totalCount={totalCount}
-            />
-
-            <ErrorBoundary logId={TABLE_PAGE_LAYOUT_LOG_ID}>
-              <div className="overflow-x-scroll">{table}</div>
-            </ErrorBoundary>
-
-            <div className="px-sds-xl">
-              {filteredCount === 0 && (
-                <div className="mt-[100px]">{noFilteredResults}</div>
-              )}
-
-              {filteredCount > MAX_PER_PAGE && (
-                <div
-                  className="w-full flex justify-center mt-sds-xxl"
-                  data-testid={TestIds.Pagination}
-                >
-                  <Pagination
-                    currentPage={pageQueryParamValue}
-                    pageSize={MAX_PER_PAGE}
-                    totalCount={
-                      totalCount === filteredCount ? totalCount : filteredCount
-                    }
-                    onNextPage={() => setPage(pageQueryParamValue + 1)}
-                    onPreviousPage={() => setPage(pageQueryParamValue - 1)}
-                    onPageChange={(nextPage) => setPage(nextPage)}
-                  />
-                </div>
-              )}
-            </div>
+            {filteredCount > MAX_PER_PAGE && (
+              <div
+                className="w-full flex justify-center mt-sds-xxl"
+                data-testid={TestIds.Pagination}
+              >
+                <Pagination
+                  currentPage={pageQueryParamValue}
+                  pageSize={MAX_PER_PAGE}
+                  totalCount={
+                    totalCount === filteredCount ? totalCount : filteredCount
+                  }
+                  onNextPage={() => setPage(pageQueryParamValue + 1)}
+                  onPreviousPage={() => setPage(pageQueryParamValue - 1)}
+                  onPageChange={(nextPage) => setPage(nextPage)}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </LayoutContext.Provider>
+    </div>
   )
 }
