58e36eaffb53422e4796254ec907fe2c15aa73b7
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2024-11-06 10:41:22 -0800

    feat: definition and search updates (#1298)
    #1267
    
    - Refactors the table header into a separate component
    - Adds new table definition header component for increasing visibility
    of the dataset / deposition description
    
    ## Demos
    
    ### Browse Datasets
    
    https://dev-table-def.cryoet.dev.si.czi.technology/browse-data/datasets
    
    <img width="1728" alt="image"
    src="https://github.com/user-attachments/assets/51702ce4-6577-4183-aa8f-3e89e788f13a">
    
    ### Browse Depositions
    
    
    https://dev-table-def.cryoet.dev.si.czi.technology/browse-data/depositions
    
    <img width="1728" alt="image"
    src="https://github.com/user-attachments/assets/50f702bc-ac3d-4387-975e-4386f66e7aa1">

---
+++ frontend/packages/data-portal/app/components/TablePageLayout/TableHeaderDefinition.tsx
@@ -0,0 +1,55 @@
+import { ReactNode } from 'react'
+
+import { Link } from 'app/components/Link'
+import { useI18n } from 'app/hooks/useI18n'
+
+import { TableCount } from './TableCount'
+import { TableHeaderProps } from './TableHeader'
+
+export function TableHeaderDefinition({
+  countLabel,
+  filteredCount,
+  totalCount,
+  title,
+  description,
+  learnMoreLink,
+  search,
+}: TableHeaderProps & {
+  search?: ReactNode
+}) {
+  const { t } = useI18n()
+
+  return (
+    <div className="pt-sds-s px-sds-xl max-w-content">
+      <h1 className="text-sds-header-xxl leading-sds-header-xxl tracking-sds-header-xxl font-semibold">
+        {title}
+      </h1>
+
+      <p className="text-sds-body-s leading-sds-body-s tracking-sds-body-s max-w-[600px] mt-sds-s">
+        <span className="text-sds-color-semantic-text-base-secondary">
+          {description}
+        </span>
+
+        {learnMoreLink && (
+          <Link
+            className="text-sds-color-semantic-text-action-default hover:text-sds-color-semantic-text-action-hover"
+            to={learnMoreLink}
+          >
+            {' '}
+            {t('learnMore')}
+          </Link>
+        )}
+      </p>
+
+      <div className="flex justify-between items-center mt-sds-xl">
+        <TableCount
+          filteredCount={filteredCount}
+          totalCount={totalCount}
+          type={countLabel}
+        />
+
+        {search}
+      </div>
+    </div>
+  )
+}
--- frontend/packages/data-portal/app/components/TablePageLayout/TablePageLayout.tsx
+++ frontend/packages/data-portal/app/components/TablePageLayout/TablePageLayout.tsx
@@ -1,19 +1,16 @@
 import { Pagination } from '@czi-sds/components'
 import { useSearchParams } from '@remix-run/react'
-import { ReactNode, useEffect, useMemo } from 'react'
+import { ComponentType, ReactNode, useEffect } from 'react'
 
-import { Link } from 'app/components/Link'
+import { ErrorBoundary } from 'app/components/ErrorBoundary'
 import { Tabs } from 'app/components/Tabs'
 import { TABLE_PAGE_LAYOUT_LOG_ID } from 'app/constants/error'
 import { MAX_PER_PAGE } from 'app/constants/pagination'
 import { QueryParams } from 'app/constants/query'
 import { TestIds } from 'app/constants/testIds'
-import { LayoutContext, LayoutContextValue } from 'app/context/Layout.context'
-import { useI18n } from 'app/hooks/useI18n'
 import { cns } from 'app/utils/cns'
 
-import { ErrorBoundary } from './ErrorBoundary'
-import { TableCount } from './Table/TableCount'
+import { TableHeader, TableHeaderProps } from './TableHeader'
 
 export interface TablePageLayoutProps {
   banner?: ReactNode
@@ -27,6 +24,7 @@ export interface TablePageLayoutProps {
 }
 
 export interface TableLayoutTab {
+  Header?: ComponentType<TableHeaderProps>
   title: string
   description?: string
   learnMoreLink?: string
@@ -118,10 +116,10 @@ function TablePageTabContent({
   totalCount,
   countLabel,
   banner,
+  Header = TableHeader,
 }: TableLayoutTab) {
   const [searchParams, setSearchParams] = useSearchParams()
   const pageQueryParamValue = +(searchParams.get(pageQueryParamKey) ?? '1')
-  const { t } = useI18n()
 
   useEffect(() => {
     if (Math.ceil(filteredCount / MAX_PER_PAGE) < pageQueryParamValue) {
@@ -142,114 +140,82 @@ function TablePageTabContent({
     })
   }
 
-  const contextValue = useMemo<LayoutContextValue>(
-    () => ({
-      hasFilters: !!filterPanel,
-    }),
-    [filterPanel],
-  )
-
   if (noTotalResults !== undefined && totalCount === 0) {
     return noTotalResults
   }
 
   return (
-    <LayoutContext.Provider value={contextValue}>
-      <div className="flex flex-auto">
-        {filterPanel && (
-          <div
-            className={cns(
-              'flex flex-col flex-shrink-0 w-[235px]',
-              'border-t border-r border-sds-color-primitive-gray-300',
-            )}
-          >
-            {filterPanel}
-          </div>
-        )}
-
+    <div className="flex flex-auto">
+      {filterPanel && (
         <div
           className={cns(
-            'flex flex-col flex-auto',
-            'pb-sds-xxl',
-            'border-t border-sds-color-primitive-gray-300',
-            'overflow-x-scroll max-w-full',
-            !banner && 'pt-sds-xl',
-
-            filterPanel
-              ? 'screen-2040:items-center'
-              : 'screen-1024:items-center',
+            'flex flex-col flex-shrink-0 w-[235px]',
+            'border-t border-r border-sds-color-primitive-gray-300',
           )}
         >
-          <div
-            className={cns(
-              'flex flex-col flex-auto w-full max-w-content',
+          {filterPanel}
+        </div>
+      )}
 
-              // Translate to the left by half the filter panel width to align with the header
-              filterPanel && 'screen-2040:translate-x-[-117.5px]',
+      <div
+        className={cns(
+          'flex flex-col flex-auto',
+          'pb-sds-xxl',
+          'border-t border-sds-color-primitive-gray-300',
+          'overflow-x-scroll max-w-full',
+          !banner && 'pt-sds-xl',
+
+          filterPanel ? 'screen-2040:items-center' : 'screen-1024:items-center',
+        )}
+      >
+        <div
+          className={cns(
+            'flex flex-col flex-auto w-full max-w-content',
+
+            // Translate to the left by half the filter panel width to align with the header
+            filterPanel && 'screen-2040:translate-x-[-117.5px]',
+          )}
+        >
+          {banner && <div className="flex px-sds-xl">{banner}</div>}
+
+          <Header
+            countLabel={countLabel}
+            description={description}
+            filteredCount={filteredCount}
+            learnMoreLink={learnMoreLink}
+            title={title}
+            totalCount={totalCount}
+          />
+
+          <ErrorBoundary logId={TABLE_PAGE_LAYOUT_LOG_ID}>
+            <div className="overflow-x-scroll">{table}</div>
+          </ErrorBoundary>
+
+          <div className="px-sds-xl">
+            {filteredCount === 0 && (
+              <div className="mt-[100px]">{noFilteredResults}</div>
             )}
-          >
-            {banner && <div className="flex px-sds-xl">{banner}</div>}
 
-            <div
-              className={cns(
-                'ml-sds-xl p-sds-m flex items-center gap-x-sds-xl  screen-1024:mr-sds-xl',
-                // NOTE: The title background color is gray on the browse datasets and browse depositions pages
-                // If we want to add a description to the single deposition or single run pages, we may need to update this
-                !!description && 'bg-sds-color-primitive-gray-100',
-              )}
-            >
-              <p className="text-sds-header-l leading-sds-header-l font-semibold">
-                {title}
-              </p>
-
-              <TableCount
-                filteredCount={filteredCount}
-                totalCount={totalCount}
-                type={countLabel}
-              />
-              <p className="inline">
-                {description}
-                {learnMoreLink && (
-                  <Link
-                    to={learnMoreLink}
-                    className="text-sds-color-primitive-blue-400 ml-sds-xxs"
-                  >
-                    {t('learnMore')}
-                  </Link>
-                )}
-              </p>
-            </div>
-
-            <ErrorBoundary logId={TABLE_PAGE_LAYOUT_LOG_ID}>
-              <div className="overflow-x-scroll">{table}</div>
-            </ErrorBoundary>
-
-            <div className="px-sds-xl">
-              {filteredCount === 0 && (
-                <div className="mt-[100px]">{noFilteredResults}</div>
-              )}
-
-              {filteredCount > MAX_PER_PAGE && (
-                <div
-                  className="w-full flex justify-center mt-sds-xxl"
-                  data-testid={TestIds.Pagination}
-                >
-                  <Pagination
-                    currentPage={pageQueryParamValue}
-                    pageSize={MAX_PER_PAGE}
-                    totalCount={
-                      totalCount === filteredCount ? totalCount : filteredCount
-                    }
-                    onNextPage={() => setPage(pageQueryParamValue + 1)}
-                    onPreviousPage={() => setPage(pageQueryParamValue - 1)}
-                    onPageChange={(nextPage) => setPage(nextPage)}
-                  />
-                </div>
-              )}
-            </div>
+            {filteredCount > MAX_PER_PAGE && (
+              <div
+                className="w-full flex justify-center mt-sds-xxl"
+                data-testid={TestIds.Pagination}
+              >
+                <Pagination
+                  currentPage={pageQueryParamValue}
+                  pageSize={MAX_PER_PAGE}
+                  totalCount={
+                    totalCount === filteredCount ? totalCount : filteredCount
+                  }
+                  onNextPage={() => setPage(pageQueryParamValue + 1)}
+                  onPreviousPage={() => setPage(pageQueryParamValue - 1)}
+                  onPageChange={(nextPage) => setPage(nextPage)}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </LayoutContext.Provider>
+    </div>
   )
 }
