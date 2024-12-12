8eb179ab41ce707858e38f3791bdd4b0e92e7e81
Jeremy <jeremyasuncion808@gmail.com>
2024-12-11 17:08:00 -0800

    fix type errors

--- frontend/packages/data-portal/e2e/apollo.ts
+++ frontend/packages/data-portal/e2e/apollo.ts
@@ -2,7 +2,7 @@
  * Version of `apollo.server.ts` that is safe to use in e2e tests.
  */
 
-import apollo from '@apollo/client'
+import * as apollo from '@apollo/client'
 
 import { ENVIRONMENT_CONTEXT_DEFAULT_VALUE } from '../app/context/Environment.context'
 
