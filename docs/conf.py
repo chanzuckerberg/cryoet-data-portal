fc1f99407fd973053c8afd4f75fc5b48af08bdf8
melissawm <melissawm@gmail.com>
2024-12-13 16:01:03 -0300

    feat: Create versioned documentation pages
    Will require a reorg of the gh-pages branch to work correctly.

--- docs/conf.py
+++ docs/conf.py
@@ -110,6 +110,19 @@ html_theme_options = {
         "text": "Inter",  # used for all the pages' text
         "code": "Roboto Mono",  # used for literal code blocks
     },
+    "version_dropdown": True,
+    "version_info": [
+        {
+            "version": "dev", # version number or path
+            "title": "Latest",
+            "aliases": [],
+        },
+        {
+            "version": "stable",
+            "title": "Stable",
+            "aliases": [],
+        },
+    ],
     # "analytics": {"provider": "google", "property": "G-XXXXXXXXXX"},
 }
 
