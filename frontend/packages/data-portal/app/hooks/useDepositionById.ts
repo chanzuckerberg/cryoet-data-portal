529422a5a3995dd6f8a54ae2249e20a0306dc343
Bryan Chu <bchu@chanzuckerberg.com>
2025-01-13 15:28:48 -0800

    v1

--- frontend/packages/data-portal/app/hooks/useDepositionById.ts
+++ frontend/packages/data-portal/app/hooks/useDepositionById.ts
@@ -8,7 +8,7 @@ export type Deposition = NonNullable<GetDepositionByIdQuery['deposition']>
 export type Dataset = GetDepositionByIdQuery['datasets'][number]
 
 export function useDepositionById() {
-  const { v1: data, annotationMethodCounts } = useTypedLoaderData<{
+  const { v1, annotationMethodCounts } = useTypedLoaderData<{
     v1: GetDepositionByIdQuery
     annotationMethodCounts: Map<string, number>
   }>()
@@ -17,40 +17,39 @@ export function useDepositionById() {
     () =>
       Array.from(
         new Set(
-          data.deposition?.object_names.flatMap(
+          v1.deposition?.object_names.flatMap(
             (annotation) => annotation.object_name,
           ),
         ),
       ),
-    [data.deposition?.object_names],
+    [v1.deposition?.object_names],
   )
 
   const objectShapeTypes = useMemo(
     () =>
       Array.from(
         new Set(
-          data.deposition?.annotations.flatMap((annotation) =>
+          v1.deposition?.annotations.flatMap((annotation) =>
             annotation.files.flatMap((file) => file.shape_type),
           ),
         ),
       ),
-    [data.deposition?.annotations],
+    [v1.deposition?.annotations],
   )
 
   const organismNames = useMemo(
     () =>
       Array.from(
-        new Set(data.datasets.flatMap((dataset) => dataset.organism_name)),
+        new Set(v1.datasets.flatMap((dataset) => dataset.organism_name)),
       ).filter(Boolean) as string[],
-    [data.datasets],
+    [v1.datasets],
   )
 
   return {
-    deposition: data.deposition as Deposition,
-    datasets: data.datasets,
-    datasetsCount: data.datasets_aggregate.aggregate?.count ?? 0,
-    filteredDatasetsCount:
-      data.filtered_datasets_aggregate.aggregate?.count ?? 0,
+    deposition: v1.deposition as Deposition,
+    datasets: v1.datasets,
+    datasetsCount: v1.datasets_aggregate.aggregate?.count ?? 0,
+    filteredDatasetsCount: v1.filtered_datasets_aggregate.aggregate?.count ?? 0,
     objectNames,
     objectShapeTypes,
     organismNames,
