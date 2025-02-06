cbf2ce15d73ae8eaf8cc3a36a224efdeef2681a5
Bryan Chu <151689101+bchu1@users.noreply.github.com>
2025-02-06 12:08:18 -0800

    feat: Stop querying V1 (#1582)
    * delete dpeositons

--- frontend/packages/data-portal/app/routes/browse-data.datasets.tsx
+++ frontend/packages/data-portal/app/routes/browse-data.datasets.tsx
@@ -1,9 +1,8 @@
 import { CellHeaderDirection } from '@czi-sds/components'
 import { json, LoaderFunctionArgs } from '@remix-run/node'
 
-import { Order_By } from 'app/__generated__/graphql'
 import { OrderBy } from 'app/__generated_v2__/graphql'
-import { apolloClient, apolloClientV2 } from 'app/apollo.server'
+import { apolloClientV2 } from 'app/apollo.server'
 import { DatasetTable } from 'app/components/BrowseData'
 import { BrowseDataSearch } from 'app/components/BrowseData/BrowseDataSearch'
 import { DatasetFilter } from 'app/components/DatasetFilter'
@@ -15,9 +14,6 @@ import {
 } from 'app/components/TablePageLayout'
 import { DATASET_FILTERS } from 'app/constants/filterQueryParams'
 import { QueryParams } from 'app/constants/query'
-import { getBrowseDatasets } from 'app/graphql/getBrowseDatasets.server'
-import { logIfHasDiff } from 'app/graphql/getDatasetsDiffer'
-import { getDatasetsFilterData } from 'app/graphql/getDatasetsFilterData.server'
 import { getDatasetsV2 } from 'app/graphql/getDatasetsV2.server'
 import { useDatasetsFilterData } from 'app/hooks/useDatasetsFilterData'
 import { useI18n } from 'app/hooks/useI18n'
@@ -34,46 +30,21 @@ export async function loader({ request }: LoaderFunctionArgs) {
     | undefined
   const query = url.searchParams.get(QueryParams.Search) ?? ''
 
-  let orderBy: Order_By | null = null
   let orderByV2: OrderBy | undefined
 
   if (sort) {
-    orderBy = sort === 'asc' ? Order_By.Asc : Order_By.Desc
     orderByV2 = sort === 'asc' ? OrderBy.Asc : OrderBy.Desc
   }
 
-  const [
-    { data: responseV1 },
-    { data: filterValuesResponseV1 },
-    { data: responseV2 },
-  ] = await Promise.all([
-    getBrowseDatasets({
-      orderBy,
-      page,
-      query,
-      client: apolloClient,
-      params: url.searchParams,
-    }),
-    getDatasetsFilterData({ client: apolloClient }),
-    getDatasetsV2({
-      page,
-      titleOrderDirection: orderByV2,
-      searchText: query,
-      params: url.searchParams,
-      client: apolloClientV2,
-    }),
-  ])
-
-  try {
-    logIfHasDiff(request.url, responseV1, filterValuesResponseV1, responseV2)
-  } catch (error) {
-    // eslint-disable-next-line no-console, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-    console.log(`DIFF ERROR: ${(error as any)?.stack}`)
-  }
+  const { data: responseV2 } = await getDatasetsV2({
+    page,
+    titleOrderDirection: orderByV2,
+    searchText: query,
+    params: url.searchParams,
+    client: apolloClientV2,
+  })
 
   return json({
-    v1: responseV1,
-    v1FilterValues: filterValuesResponseV1,
     v2: responseV2,
   })
 }
