ca7299db7b76aa2492fc04efcfcdfe922e623f02
Melissa Weber Mendonça <melissawm@gmail.com>
2025-02-07 18:32:39 -0300

    feat: Add announcement banner for API v3 deprecation

--- frontend/packages/data-portal/app/constants/localStorage.ts
+++ frontend/packages/data-portal/app/constants/localStorage.ts
@@ -2,6 +2,7 @@ export enum LocalStorageKeys {
   CompetitionEndingBannerDismissed = 'competition-ending-banner-dismissed',
   SurveyBannerDismissed = 'survey-banner-dismissed',
   TableRenderErrorPageReloadCount = 'table-render-error-page-reload-count',
+  AnnouncementBannerDismissed = 'announcement-dismissed',
   // DEPRECATED - DO NOT USE (keep these in the enum so we know not to use them in the future):
   CompetitionBannerDismissed = 'competition-banner-dismissed',
 }
