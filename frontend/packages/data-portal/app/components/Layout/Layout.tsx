6eaa9e872d4ebb0e20397eb063ce92d28925ddc9
Suzette McCanny <smccanny@deloitte.ca>
2025-02-13 10:47:05 -0500

    scroll window on drawer open

--- frontend/packages/data-portal/app/components/Layout/Layout.tsx
+++ frontend/packages/data-portal/app/components/Layout/Layout.tsx
@@ -1,12 +1,21 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useRef } from 'react'
 
 import { SurveyBanner } from 'app/components/SurveyBanner'
+import { useMetadataDrawer } from 'app/hooks/useMetadataDrawer'
 
 import { AnnouncementBanner } from '../Announcement'
 import { Footer } from './Footer'
 import { TopNavigation } from './TopNavigation'
 
 export function Layout({ children }: { children: ReactNode }) {
+  const { activeDrawer } = useMetadataDrawer()
+  const myRef = useRef(null)
+  useEffect(() => {
+    console.log(activeDrawer !== null)
+    if (activeDrawer !== null) {
+      window.scrollTo(0, 40)
+    }
+  }, [activeDrawer])
   return (
     <main className="flex flex-col flex-auto">
       {/* TODO: Add back in to announce winners! */}
