3a076e898e009b8a5ae3609db4c3c96f1352fb51
Melissa Weber Mendonça <melissawm@gmail.com>
2025-02-27 09:46:45 -0300

    chore: Remove deprecation banner from Portal UI

--- frontend/packages/data-portal/app/components/Layout/Layout.tsx
+++ frontend/packages/data-portal/app/components/Layout/Layout.tsx
@@ -1,21 +1,13 @@
 import { ReactNode } from 'react'
 
 import { SurveyBanner } from 'app/components/SurveyBanner'
-import { LocalStorageKeys } from 'app/constants/localStorage'
 
-import { ReusableBanner } from '../ReusableBanner/ReusableBanner'
 import { Footer } from './Footer'
 import { TopNavigation } from './TopNavigation'
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <main className="flex flex-col flex-auto">
-      <ReusableBanner
-        bannerTextKey="deprecatedApiBannerText"
-        localStorageKey={LocalStorageKeys.PythonV3DeprecatedDismissed}
-        allowedPathsRegex={[/^(?!\/competition).*$/]}
-        className="!bg-[#fff3db] !text-black [&_svg]:!fill-black"
-      />
       <TopNavigation />
       <div className="flex flex-col flex-[1_0_auto]">{children}</div>
       <Footer />
