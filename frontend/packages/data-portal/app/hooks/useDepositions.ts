81c52b4bb71575e2d10dfb6d00a7f6949cc57102
Suzette McCanny <smccanny@deloitte.ca>
2025-03-03 13:01:09 -0500

    fix: fix depositions asc sort (#1664)
    fix depositions asc sort

--- frontend/packages/data-portal/app/hooks/useDepositions.ts
+++ frontend/packages/data-portal/app/hooks/useDepositions.ts
@@ -1,15 +1,21 @@
 import { useMemo } from 'react'
 import { useTypedLoaderData } from 'remix-typedjson'
 
-import { GetDepositionsDataV2Query } from 'app/__generated_v2__/graphql'
+import {
+  GetDepositionsDataV2Query,
+  OrderBy,
+} from 'app/__generated_v2__/graphql'
 import { remapV2BrowseAllDepositions } from 'app/apiNormalization'
 
 export function useDepositions() {
-  const { v2 } = useTypedLoaderData<{
+  const { v2, orderBy } = useTypedLoaderData<{
     v2: GetDepositionsDataV2Query
+    orderBy: OrderBy
   }>()
-
-  const v2result = useMemo(() => remapV2BrowseAllDepositions(v2), [v2])
+  const v2result = useMemo(
+    () => remapV2BrowseAllDepositions(v2, orderBy),
+    [v2, orderBy],
+  )
 
   return v2result
 }
