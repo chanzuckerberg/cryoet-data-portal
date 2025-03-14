9323769945e247b1a490b952c779ad33d16e7d9d
Suzette McCanny <smccanny@deloitte.ca>
2025-03-13 23:09:08 -0400

    add gt tag to dataset table

--- frontend/packages/data-portal/app/components/BrowseData/DatasetTable.tsx
+++ frontend/packages/data-portal/app/components/BrowseData/DatasetTable.tsx
@@ -31,7 +31,6 @@ import { Dataset } from 'app/types/gql/datasetsPageTypes'
 import { LogLevel } from 'app/types/logging'
 import { cnsNoMerge } from 'app/utils/cns'
 import { sendLogs } from 'app/utils/logging'
-import { isDefined } from 'app/utils/nullish'
 import { getErrorMessage } from 'app/utils/string'
 import { carryOverFilterParams, createUrl } from 'app/utils/url'
 
@@ -268,9 +267,21 @@ export function DatasetTable() {
 
         columnHelper.accessor(
           (dataset) =>
-            dataset.distinctObjectNames?.aggregate
-              ?.map((aggregate) => aggregate.groupBy?.annotations?.objectName)
-              .filter(isDefined) ?? [],
+            dataset.distinctObjectNames?.aggregate?.reduce((acc, aggregate) => {
+              const objectName = aggregate?.groupBy?.annotations?.objectName
+              const groundTruthStatus =
+                aggregate?.groupBy?.annotations?.groundTruthStatus ?? false
+              if (!objectName) return acc // Skip invalid entries
+              if (acc.has(objectName)) {
+                // If the objectName is already in the map
+                if (groundTruthStatus) {
+                  acc.set(objectName, true) // if any runs have the ground truth status, set the annotatedObject to true
+                }
+              } else {
+                acc.set(objectName, groundTruthStatus)
+              }
+              return acc
+            }, new Map<string, boolean>()) ?? [],
           {
             id: 'annotatedObjects',
 
