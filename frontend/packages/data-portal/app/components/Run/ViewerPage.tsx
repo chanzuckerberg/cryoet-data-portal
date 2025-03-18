6bfb424c758a594fda4485b39c79d62298a49e12
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2025-03-18 18:20:42 +0100

    CC-218 change the way how to render component

--- frontend/packages/data-portal/app/components/Run/ViewerPage.tsx
+++ frontend/packages/data-portal/app/components/Run/ViewerPage.tsx
@@ -1,14 +1,16 @@
 import './ViewerPage.css'
 
 import { currentState, NeuroglancerWrapper, updateState } from 'neuroglancer'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { cns } from 'app/utils/cns'
 import { CryoETHomeLink } from '../Layout/CryoETHomeLink'
 import { Breadcrumbs } from 'app/components/Breadcrumbs'
 import { Button } from '@czi-sds/components'
 import { InfoIcon } from 'app/components/icons'
-import { CustomDropdown } from '../common/CustomDropdown'
+import { MenuItemLink } from "app/components/MenuItemLink";
+import { CustomDropdown, CustomDropdownSection, CustomDropdownOption } from '../common/CustomDropdown'
 import { ABOUT_LINKS, HELP_AND_REPORT_LINKS, NEUROGLANCER_HELP_LINKS } from '../Layout/constants'
+import { useI18n } from 'app/hooks/useI18n'
 
 // Button action for toggling layers visibility
 const isAnnotation = (layer: any) =>
@@ -16,14 +18,14 @@ const isAnnotation = (layer: any) =>
 const toggleVisibility = (layer: any) =>
   !(layer.visible === undefined || layer.visible)
 
-const toggleLayersVisibility = () => {
-  updateState((state) => {
-    for (const layer of state.neuroglancer.layers) {
-      layer.visible = toggleVisibility(layer)
-    }
-    return state
-  })
-}
+// const toggleLayersVisibility = () => {
+//   updateState((state) => {
+//     for (const layer of state.neuroglancer.layers) {
+//       layer.visible = toggleVisibility(layer)
+//     }
+//     return state
+//   })
+// }
 
 const toggleAnnotations = () => {
   updateState((state) => {
@@ -36,19 +38,35 @@ const toggleAnnotations = () => {
   })
 }
 
+const toggleBoundingBox = () => {
+  updateState((state) => {
+    state.neuroglancer.showDefaultAnnotations = toggleAnnotations();
+    return state
+  })
+}
+
 const hasAnnotationLayers = (state: any) => {
   const root = state.neuroglancer || state
   return root.layers.some(isAnnotation)
 }
 
 function ViewerPage({ run } : { run: any }) {
+  const { t } = useI18n()
+  const state = currentState().neuroglancer;
   const [hasAnnotations, setHasAnnotations] = useState(
     hasAnnotationLayers(currentState()),
   )
+  const [annotations, setAnnotations] = useState<any>([])
 
   const updateButtons = (state: any) => {
     setHasAnnotations(hasAnnotationLayers(state))
   }
+
+  useEffect(() => {
+    const filteredAnnotations = state.layers.filter((layer: any) => layer.type === "annotation");
+
+    setAnnotations(filteredAnnotations);
+  }, []);
 
   const activeBreadcrumbText = (
     <p>
@@ -72,75 +90,69 @@ function ViewerPage({ run } : { run: any }) {
         <div className="basis-sds-xxl flex-grow screen-790:mr-sds-xxl" />
         <div className="hidden screen-716:flex basis-auto flex-shrink-0">
           <div className="flex items-center gap-1">
-            <CustomDropdown 
-              title="Annotations" 
-              variant="outlined"
-              sections={[
-                {
-                  title: "Toggle annotations per deposition",
-                  options: [
-                    {label: "All annotations", checked: false},
-                    {label: "Deposition #1", subLabel: "#CZCDP-12795", checked: false},
-                    {label: "Deposition #2", subLabel: "#CZCDP-12796", checked: false},
-                    {label: "Deposition #3", subLabel: "#CZCDP-12797", checked: false}
-                  ]
-                }
-              ]}
-            />
-            <CustomDropdown 
-              title="Layout" 
-              variant="outlined"
-              sections={[
-                {
-                  title: "Layout", 
-                  options: [
-                    {label: "4 panel", checked: false}, 
-                    {label: "XY", checked: false}, 
-                    {label: "XZ", checked: false},
-                    {label: "YZ", checked: false},
-                    {label: "3D", checked: false}
-                  ]
-                },
-                {
-                  title: "Toggle Panels",
-                  options: [
-                    {label: "All panels", checked: false},
-                    {label: "Top layer bar", checked: false}
-                  ]
-                }
-              ]}
-            />
-            <CustomDropdown 
-              title="Actions" 
-              variant="outlined"
-              sections={[
-                {
-                  title: "Appearance",
-                  options: [
-                    {label: "Bounding box", checked: false},
-                    {label: "Axis lines", checked: false},
-                    {label: "Scale bar", checked: false},
-                    {label: "Change background to white", checked: false}
-                  ]
-                },
-                {
-                  title: "Move",
-                  options: [
-                    {label: "Snap to the nearest axis", checked: false}
-                  ]
-                }
-              ]}
-            />
+            <CustomDropdown title="Annotations" variant="outlined">
+              <CustomDropdownSection title="Toggle annotations per deposition">
+                <CustomDropdownOption selected={false} onSelect={() => toggleAnnotations()}>All annotations</CustomDropdownOption>
+                {annotations.map((annotation: any) => (
+                  <CustomDropdownOption
+                    key={annotation?.name}
+                    selected={true}
+                    onSelect={() => console.log(annotation)}
+                  >
+                    <span>{annotation?.name}</span>
+                    <span className="text-xs text-[#767676] font-normal">#CZCDP-12795</span>
+                  </CustomDropdownOption>
+                ))}
+              </CustomDropdownSection>
+            </CustomDropdown>
+            <CustomDropdown title="Layout" variant="outlined">
+              <CustomDropdownSection title="Layout">
+                <CustomDropdownOption selected={false} onSelect={() => console.log("4 panel")}>4 panel</CustomDropdownOption>
+                <CustomDropdownOption selected={false} onSelect={() => console.log("XY")}>XY</CustomDropdownOption>
+                <CustomDropdownOption selected={false} onSelect={() => console.log("XZ")}>XZ</CustomDropdownOption>
+                <CustomDropdownOption selected={false} onSelect={() => console.log("YZ")}>YZ</CustomDropdownOption>
+                <CustomDropdownOption selected={false} onSelect={() => console.log("3D")}>3D</CustomDropdownOption>
+              </CustomDropdownSection>
+              <CustomDropdownSection title="Toggle Panels">
+                <CustomDropdownOption selected={false} onSelect={() => console.log("All panels")}>All panels</CustomDropdownOption>
+                <CustomDropdownOption selected={false} onSelect={() => console.log("Top layer bar")}>Top layer bar</CustomDropdownOption>
+              </CustomDropdownSection>
+            </CustomDropdown>
+            <CustomDropdown title="Actions" variant="outlined">
+              <CustomDropdownSection title="Appearance">
+                <CustomDropdownOption selected={false} onSelect={() => toggleBoundingBox()}>Bounding box</CustomDropdownOption>
+                <CustomDropdownOption selected={false} onSelect={() => console.log("Axis lines")}>Axis lines</CustomDropdownOption>
+                <CustomDropdownOption selected={false} onSelect={() => console.log("Scale bar")}>Scale bar</CustomDropdownOption>
+                <CustomDropdownOption selected={false} onSelect={() => console.log("Change background to white")}>Change background to white</CustomDropdownOption>
+              </CustomDropdownSection>
+              <CustomDropdownSection title="Move">
+                <CustomDropdownOption selected={false} onSelect={() => console.log("Snap to the nearest axis")}>Snap to the nearest axis</CustomDropdownOption>
+              </CustomDropdownSection>
+            </CustomDropdown>
             <Button sdsType="primary" sdsStyle="rounded">Share</Button>
-            <CustomDropdown
-              buttonElement={<InfoIcon className="w-5 h-5" />}
-              className='w-11 h-11 p-3'
-              sections={[
-                {title: "About", links: ABOUT_LINKS},
-                {title: "Help & Support", links: HELP_AND_REPORT_LINKS},
-                {title: "Neuroglancer help", links: NEUROGLANCER_HELP_LINKS}
-              ]}
-            />
+            <CustomDropdown className='w-11 h-11 p-3' buttonElement={<InfoIcon className="w-5 h-5" />}>
+              <CustomDropdownSection title="About">
+                {ABOUT_LINKS.map((option) => (
+                  <MenuItemLink key={option.label} to={option.link}>
+                    {t(option.label)}
+                  </MenuItemLink>
+                ))}
+              </CustomDropdownSection>
+              <CustomDropdownSection title="Help & Support">
+                {HELP_AND_REPORT_LINKS.map((option) => (
+                  <MenuItemLink key={option.label} to={option.link}>
+                    {t(option.label)}
+                  </MenuItemLink>
+                ))}
+              </CustomDropdownSection>
+              <CustomDropdownSection title="Neuroglancer help">
+                {NEUROGLANCER_HELP_LINKS.map((option) => (
+                  <MenuItemLink key={option.label} to={option.link}>
+                    {t(option.label)}
+                  </MenuItemLink>
+                ))}
+              </CustomDropdownSection>
+            </CustomDropdown>
           </div>
         </div>
       </nav>
--- frontend/packages/data-portal/app/components/common/CustomDropdown.tsx
+++ frontend/packages/data-portal/app/components/common/CustomDropdown.tsx
@@ -1,35 +1,28 @@
-import { useState } from "react";
-import { MenuItem, Icon } from "@czi-sds/components";
-import { ComponentProps, ReactNode } from "react";
+import { ReactNode, ComponentProps } from "react";
 import { MenuDropdown } from "app/components/MenuDropdown";
 import { MenuItemHeader } from "app/components/MenuItemHeader";
-import { MenuItemLink } from "app/components/MenuItemLink";
-import { NavLink } from "../Layout/constants";
-import { useI18n } from 'app/hooks/useI18n'
-
-type OptionItem = {
-  label: string;
-  checked: boolean;
-  subLabel?: string;
-};
-
-type Section = {
-  title?: string;
-  links?: NavLink[];
-  options?: OptionItem[];
-};
+import { MenuItem, Icon } from "@czi-sds/components";
 
 type CustomDropdownProps = {
   className?: string;
   title?: string;
-  sections: Section[];
   variant?: "standard" | "outlined" | "filled";
   buttonElement?: ReactNode;
+  children: ReactNode;
 };
 
-function MenuItemOption({
+export function CustomDropdownSection({ title, children } : { title: string, children: ReactNode }) {
+  return (
+    <div>
+      {title && <MenuItemHeader>{title}</MenuItemHeader>}
+      {children}
+    </div>
+  );
+}
+
+export function CustomDropdownOption({
   selected,
-  onClick,
+  onSelect,
   children,
   ...props
 }: ComponentProps<typeof MenuItem> & {
@@ -37,70 +30,30 @@ function MenuItemOption({
   onClick?: () => void;
 }) {
   return (
-    <MenuItem {...props} onClick={onClick}>
+    <MenuItem {...props} onClick={onSelect}>
       <div className="flex items-center justify-center flex-auto gap-3">
-        <div className="inline-block w-4 h-4">
+        <div className="inline-flex w-4 h-4">
           {selected ? (
             <Icon sdsIcon="Check" sdsType="button" sdsSize="s" className="!fill-[#0B68F8]" />
           ) : null}
         </div>
-        <div className={`flex-auto ${selected && "font-semibold"}`}>{children}</div>
+        <div className={`flex flex-col ${selected && "font-semibold"}`}>{children}</div>
       </div>
     </MenuItem>
   );
 }
 
-
-
 export function CustomDropdown({
   className,
   title,
   variant = "standard",
-  sections: initialSections,
   buttonElement,
+  children
 }: CustomDropdownProps) {
-  const { t } = useI18n()
-  const [sections, setSections] = useState(initialSections);
-
-  const handleToggle = (sectionIndex: number, optionIndex: number) => {
-    setSections((prevSections) =>
-      prevSections.map((section, sIdx) =>
-        sIdx === sectionIndex
-          ? {
-              ...section,
-              options: section.options?.map((option, oIdx) =>
-                oIdx === optionIndex ? { ...option, checked: !option.checked } : option
-              ),
-            }
-          : section
-      )
-    );
-  };
-
+  
   return (
     <MenuDropdown className={className} title={title} variant={variant} buttonElement={buttonElement}>
-      {sections.map((section, sectionIndex) => (
-        <div key={sectionIndex}>
-          {section.title && <MenuItemHeader>{section.title}</MenuItemHeader>}
-
-          {section.links &&
-            section.links.map(({ label, link }) => (
-              <MenuItemLink key={label} to={link}>
-                {t(label)}
-              </MenuItemLink>
-          ))}
-
-          {section.options &&
-            section.options.map(({ label, checked, subLabel }, optionIndex) => (
-              <MenuItemOption key={label} selected={checked} onClick={() => handleToggle(sectionIndex, optionIndex)}>
-                <div className="flex flex-col">
-                  <span>{label}</span>
-                  {subLabel && <span className="text-xs text-[#767676] font-normal">{subLabel}</span>}
-                </div>
-              </MenuItemOption>
-          ))}
-        </div>
-      ))}
+      {children}
     </MenuDropdown>
   );
 }
