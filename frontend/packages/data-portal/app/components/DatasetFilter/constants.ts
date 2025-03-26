9e3932972df1c3789fb1c61d8bc269cc4515bb86
kaloster <kaloster@users.noreply.github.com>
2025-03-26 19:10:42 +0000

    Address feedback

--- frontend/packages/data-portal/app/components/DatasetFilter/constants.ts
+++ frontend/packages/data-portal/app/components/DatasetFilter/constants.ts
@@ -6,8 +6,8 @@ export const AVAILABLE_FILES_VALUE_TO_I18N_MAP: Record<
   I18nKeys
 > = {
   annotation: 'annotation',
+  tomogram: 'tomograms',
   'raw-frames': 'rawFrames',
   'tilt-series': 'tiltSeries',
   'tilt-series-alignment': 'tiltSeriesAlignment',
-  tomogram: 'tomograms',
 }
