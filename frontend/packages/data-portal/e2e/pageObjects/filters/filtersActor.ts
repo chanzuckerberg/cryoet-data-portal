49201db42f774b594dc3f84ae428c69b4202e472
Jeremy <jeremyasuncion808@gmail.com>
2025-04-01 10:57:45 -0700

    browse datasets tests

--- frontend/packages/data-portal/e2e/pageObjects/filters/filtersActor.ts
+++ frontend/packages/data-portal/e2e/pageObjects/filters/filtersActor.ts
@@ -1,6 +1,8 @@
 import { waitForTableReload } from 'e2e/utils'
 
 import { FiltersPage } from './filtersPage'
+import { MultiInputFilterType, QueryParamObjectType } from './types'
+import { getExpectedUrlWithQueryParams } from './utils'
 
 export class FiltersActor {
   private filtersPage: FiltersPage
@@ -9,23 +11,24 @@ export class FiltersActor {
     this.filtersPage = filtersPage
   }
 
-  // // #region Navigation
-  // public async goToFilteredUrl({
-  //   baseUrl,
-  //   queryParamsList,
-  //   serialize,
-  // }: {
-  //   baseUrl: string
-  //   queryParamsList: QueryParamObjectType[]
-  //   serialize?: (value: string) => string
-  // }) {
-  //   const url = this.filtersPage.getFilteredUrl({
-  //     baseUrl,
-  //     queryParamsList,
-  //     serialize,
-  //   })
-  //   await this.filtersPage.goTo(url.href)
-  // }
+  // #region Navigation
+  public async goToFilteredUrl({
+    baseUrl,
+    queryParamsList,
+    serialize,
+  }: {
+    baseUrl: string
+    queryParamsList: QueryParamObjectType[]
+    serialize?: (value: string) => string
+  }) {
+    const url = this.filtersPage.getFilteredUrl({
+      baseUrl,
+      queryParamsList,
+      serialize,
+    })
+    await this.filtersPage.goTo(url.href)
+  }
+
   // // #endregion Navigation
 
   // // #region Data
@@ -146,43 +149,43 @@ export class FiltersActor {
   //   await this.filtersPage.removeFilterOption(value)
   // }
 
-  // public async addMultiInputFilter({
-  //   buttonLabel,
-  //   filter,
-  //   hasMultipleFilters,
-  // }: {
-  //   buttonLabel: string
-  //   filter: MultiInputFilterType
-  //   hasMultipleFilters: boolean
-  // }) {
-  //   await this.filtersPage.clickFilterDropdown(buttonLabel)
-  //   await this.filtersPage.fillInputFilter({
-  //     label: `${filter.label}${hasMultipleFilters ? ':' : ''}`,
-  //     value: filter.value,
-  //   })
-  //   await this.filtersPage.applyMultiInputFilter()
-  // }
+  public async addMultiInputFilter({
+    buttonLabel,
+    filter,
+    hasMultipleFilters,
+  }: {
+    buttonLabel: string
+    filter: MultiInputFilterType
+    hasMultipleFilters: boolean
+  }) {
+    await this.filtersPage.clickFilterDropdown(buttonLabel)
+    await this.filtersPage.fillInputFilter({
+      label: `${filter.label}${hasMultipleFilters ? ':' : ''}`,
+      value: filter.value,
+    })
+    await this.filtersPage.applyMultiInputFilter()
+  }
 
   // // #endregion Macro
 
-  // // #region Validate
-  // public async expectUrlQueryParamsToBeCorrect({
-  //   url,
-  //   queryParamsList,
-  //   serialize,
-  // }: {
-  //   url: string
-  //   queryParamsList: QueryParamObjectType[]
-  //   serialize?: (value: string) => string
-  // }) {
-  //   const { expectedUrl } = getExpectedUrlWithQueryParams({
-  //     url,
-  //     queryParamsList,
-  //     serialize,
-  //   })
+  // #region Validate
+  public async expectUrlQueryParamsToBeCorrect({
+    url,
+    queryParamsList,
+    serialize,
+  }: {
+    url: string
+    queryParamsList: QueryParamObjectType[]
+    serialize?: (value: string) => string
+  }) {
+    const { expectedUrl } = getExpectedUrlWithQueryParams({
+      url,
+      queryParamsList,
+      serialize,
+    })
 
-  //   await this.filtersPage.expectNavigationToMatch(expectedUrl.href)
-  // }
+    await this.filtersPage.expectNavigationToMatch(expectedUrl.href)
+  }
 
   // public async expectAnnotationsTableToBeCorrect({
   //   singleRunData,
