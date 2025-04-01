c13f6bdd1dffd4efdeb660fcf5f81d4c6a82ebcf
Jeremy <jeremyasuncion808@gmail.com>
2025-04-01 10:57:46 -0700

    pagination e2e test

--- frontend/packages/data-portal/e2e/pagination.test.ts
+++ frontend/packages/data-portal/e2e/pagination.test.ts
@@ -1,63 +1,55 @@
-// /*
-//  * NOTE: This file does not use the preferred page object pattern.
-//  * This is because we did not have time to refactor.
-//  * Please do not use this file as an example of how to write tests.
-//  */
-// import { TableValidatorOptions } from 'e2e/pageObjects/filters/types'
+/*
+ * NOTE: This file does not use the preferred page object pattern.
+ * This is because we did not have time to refactor.
+ * Please do not use this file as an example of how to write tests.
+ */
 
-// import { MAX_PER_PAGE } from 'app/constants/pagination'
-// import { getBrowseDatasets } from 'app/graphql/getBrowseDatasets.server'
-// import { getDatasetById } from 'app/graphql/getDatasetById.server'
+import { MAX_PER_PAGE } from 'app/constants/pagination'
+import { getDatasetByIdV2 } from 'app/graphql/getDatasetByIdV2.server'
+import { getDatasetsV2 } from 'app/graphql/getDatasetsV2.server'
 
-// import { BROWSE_DATASETS_URL, E2E_CONFIG, translations } from './constants'
-// import { validateDatasetsTable, validateRunsTable } from './filters/utils'
-// import { testPagination } from './pagination/testPagination'
-// import { getParamsFromFilter } from './pagination/utils'
+import { BROWSE_DATASETS_URL, E2E_CONFIG, translations } from './constants'
+import { testPagination } from './pagination/testPagination'
+import { getParamsFromFilter } from './pagination/utils'
 
-// const { pagination } = E2E_CONFIG
-// const { browseDatasets, singleDataset } = pagination
+const { pagination } = E2E_CONFIG
+const { browseDatasets, singleDataset } = pagination
 
-// testPagination({
-//   rowLoadedSelector: `:has-text("${translations.datasetId}:")`,
-//   testFilter: browseDatasets.filter,
-//   testFilterWithNoPages: browseDatasets.filterWithNoPages,
-//   url: BROWSE_DATASETS_URL,
-//   validateTable: validateDatasetsTable,
+testPagination({
+  rowLoadedSelector: `:has-text("${translations.datasetId}:")`,
+  testFilter: browseDatasets.filter,
+  testFilterWithNoPages: browseDatasets.filterWithNoPages,
+  url: BROWSE_DATASETS_URL,
 
-//   async getMaxPages(client, filter, pageNumber) {
-//     const { data } = await getBrowseDatasets({
-//       client,
-//       params: getParamsFromFilter(filter),
-//       page: pageNumber,
-//     })
-//     const count = data.filtered_datasets_aggregate.aggregate?.count ?? 0
+  async getMaxPages(client, filter, page) {
+    const { data } = await getDatasetsV2({
+      client,
+      page,
+      params: getParamsFromFilter(filter),
+    })
+    const count = data.filteredDatasetsCount.aggregate?.at(0)?.count ?? 0
 
-//     return Math.ceil(count / MAX_PER_PAGE)
-//   },
-// })
+    return Math.ceil(count / MAX_PER_PAGE)
+  },
+})
 
-// const validateRunsTableWithId = (options: TableValidatorOptions) =>
-//   validateRunsTable({
-//     ...options,
-//     id: +singleDataset.id,
-//   })
+testPagination({
+  rowLoadedSelector: `:has-text("${translations.runId}:")`,
+  testFilter: singleDataset.filter,
+  testFilterWithNoPages: singleDataset.filterWithNoPages,
+  url: `${E2E_CONFIG.url}/datasets/${singleDataset.id}`,
 
-// testPagination({
-//   rowLoadedSelector: `:has-text("${translations.runId}:")`,
-//   testFilter: singleDataset.filter,
-//   testFilterWithNoPages: singleDataset.filterWithNoPages,
-//   url: `${E2E_CONFIG.url}/datasets/${singleDataset.id}`,
-//   validateTable: validateRunsTableWithId,
+  async getMaxPages(client, filter, pageNumber) {
+    const { data } = await getDatasetByIdV2({
+      client,
+      params: getParamsFromFilter(filter),
+      page: pageNumber,
+      id: +singleDataset.id,
+    })
 
-//   async getMaxPages(client, filter, pageNumber) {
-//     const { data } = await getDatasetById({
-//       client,
-//       params: getParamsFromFilter(filter),
-//       page: pageNumber,
-//       id: +singleDataset.id,
-//     })
-//     const count = data.datasets.at(0)?.filtered_runs_count.aggregate?.count ?? 0
+    const count =
+      data.datasets.at(0)?.filteredRunsCount?.aggregate?.at(0)?.count ?? 0
 
-//     return Math.ceil(count / MAX_PER_PAGE)
-//   },
-// })
+    return Math.ceil(count / MAX_PER_PAGE)
+  },
+})
