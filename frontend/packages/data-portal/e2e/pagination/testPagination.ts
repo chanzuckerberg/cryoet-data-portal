be9ef3de2452d6927c3889994656852701193c3a
Jeremy <jeremyasuncion808@gmail.com>
2025-04-04 15:11:15 -0700

    pagination e2e test

--- frontend/packages/data-portal/e2e/pagination/testPagination.ts
+++ frontend/packages/data-portal/e2e/pagination/testPagination.ts
@@ -1,211 +1,190 @@
-// /*
-//  * NOTE: This file does not use the preferred page object pattern.
-//  * This is because we did not have time to refactor.
-//  * Please do not use this file as an example of how to write tests.
-//  */
-// import { expect, Page, test } from '@playwright/test'
-// import { getApolloClient } from 'e2e/apollo'
-// import { E2E_CONFIG } from 'e2e/constants'
-// import { goTo } from 'e2e/filters/utils'
-// import { TableValidator } from 'e2e/pageObjects/filters/types'
-// import { isNumber } from 'lodash-es'
+/*
+ * NOTE: This file does not use the preferred page object pattern.
+ * This is because we did not have time to refactor.
+ * Please do not use this file as an example of how to write tests.
+ */
+import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
+import { expect, Page, test } from '@playwright/test'
+import { getApolloClientV2 } from 'e2e/apollo'
+import { E2E_CONFIG } from 'e2e/constants'
+import { goTo } from 'e2e/filters/utils'
+import { isNumber } from 'lodash-es'
 
-// import { QueryParams } from 'app/constants/query'
-// import { TestIds } from 'app/constants/testIds'
+import { QueryParams } from 'app/constants/query'
+import { TestIds } from 'app/constants/testIds'
 
-// import {
-//   GetMaxPages,
-//   PaginationFilter,
-//   TestOptions as PaginationTestOptions,
-// } from './types'
-// import { getParamsFromFilter } from './utils'
+import {
+  GetMaxPages,
+  PaginationFilter,
+  TestOptions as PaginationTestOptions,
+} from './types'
 
-// function getPagination(page: Page) {
-//   return page.getByTestId(TestIds.Pagination)
-// }
+function getPagination(page: Page) {
+  return page.getByTestId(TestIds.Pagination)
+}
 
-// function getPreviousButton(page: Page) {
-//   return page.locator(`[data-order="first"]`)
-// }
+function getPreviousButton(page: Page) {
+  return page.locator(`[data-order="first"]`)
+}
 
-// function getNextButton(page: Page) {
-//   return page.locator(`[data-order="last"]`)
-// }
+function getNextButton(page: Page) {
+  return page.locator(`[data-order="last"]`)
+}
 
-// async function clickVisiblePageButton(page: Page, pageNumber: number) {
-//   await getPagination(page).getByText(`${pageNumber}`).click()
-// }
+async function clickVisiblePageButton(page: Page, pageNumber: number) {
+  await getPagination(page).getByText(`${pageNumber}`).click()
+}
 
-// async function openPaginationDropdown(page: Page) {
-//   await page.getByLabel('Go to a page').click()
-// }
+async function openPaginationDropdown(page: Page) {
+  await page.getByLabel('Go to a page').click()
+}
 
-// async function selectPaginationDropdownOption(page: Page, pageNumber: number) {
-//   await page.getByRole('menu').getByText(`${pageNumber}`).click()
-// }
+async function selectPaginationDropdownOption(page: Page, pageNumber: number) {
+  await page
+    .getByRole('menu')
+    .getByText(`${pageNumber}`, { exact: true })
+    .click()
+}
 
-// async function waitForPageNumber(page: Page, pageNumber: number) {
-//   await page.waitForURL(
-//     (url) => url.searchParams.get(QueryParams.Page) === `${pageNumber}`,
-//   )
-// }
+async function waitForPageNumber(page: Page, pageNumber: number) {
+  await page.waitForURL(
+    (url) => url.searchParams.get(QueryParams.Page) === `${pageNumber}`,
+  )
+}
 
-// const { pagination } = E2E_CONFIG
+const { pagination } = E2E_CONFIG
 
-// export function testPagination({
-//   getMaxPages,
-//   rowLoadedSelector,
-//   testFilter,
-//   testFilterWithNoPages,
-//   url,
-//   validateTable,
-// }: {
-//   getMaxPages: GetMaxPages
-//   rowLoadedSelector: string
-//   testFilter: PaginationFilter
-//   testFilterWithNoPages: PaginationFilter
-//   url: string
-//   validateTable: TableValidator
-// }) {
-//   test.describe(url.replace(E2E_CONFIG.url, ''), () => {
-//     let client = getApolloClient()
+export function testPagination({
+  getMaxPages,
+  rowLoadedSelector,
+  testFilter,
+  testFilterWithNoPages,
+  url,
+}: {
+  getMaxPages: GetMaxPages
+  rowLoadedSelector: string
+  testFilter: PaginationFilter
+  testFilterWithNoPages: PaginationFilter
+  url: string
+}) {
+  test.describe(url.replace(E2E_CONFIG.url, ''), () => {
+    let client: ApolloClient<NormalizedCacheObject>
 
-//     test.beforeEach(() => {
-//       client = getApolloClient()
-//     })
+    test.beforeEach(() => {
+      client = getApolloClientV2()
+    })
 
-//     async function openPage({
-//       filter,
-//       page,
-//       pageNumber,
-//     }: {
-//       filter?: PaginationFilter
-//       page: Page
-//       pageNumber?: number
-//     }) {
-//       const nextUrl = new URL(url)
+    async function openPage({
+      filter,
+      page,
+      pageNumber,
+    }: {
+      filter?: PaginationFilter
+      page: Page
+      pageNumber?: number
+    }) {
+      const nextUrl = new URL(url)
 
-//       if (filter) {
-//         nextUrl.searchParams.set(filter.key, filter.value)
-//       }
+      if (filter) {
+        nextUrl.searchParams.set(filter.key, filter.value)
+      }
 
-//       if (isNumber(pageNumber)) {
-//         nextUrl.searchParams.set('page', `${pageNumber}`)
-//       }
+      if (isNumber(pageNumber)) {
+        nextUrl.searchParams.set('page', `${pageNumber}`)
+      }
 
-//       await goTo(page, nextUrl.href)
-//     }
+      await goTo(page, nextUrl.href)
+    }
 
-//     function testMaxPageCount({ filter }: PaginationTestOptions = {}) {
-//       test(`should show correct max page count${
-//         filter ? ' when filtered' : ''
-//       }`, async ({ page }) => {
-//         await openPage({ page, filter })
+    function testMaxPageCount({ filter }: PaginationTestOptions = {}) {
+      test(`should show correct max page count${
+        filter ? ' when filtered' : ''
+      }`, async ({ page }) => {
+        await openPage({ page, filter })
 
-//         const maxPages = await getMaxPages(client, filter)
-//         await expect(getPagination(page)).toContainText(`${maxPages}`)
-//       })
-//     }
+        const maxPages = await getMaxPages(client, filter)
+        await expect(getPagination(page)).toContainText(`${maxPages}`)
+      })
+    }
 
-//     function testSelectPage({ pageNumber = 1, filter }: PaginationTestOptions) {
-//       test(`should load correct items when selecting a page${
-//         filter ? ' and is filtered' : ''
-//       }`, async ({ page }) => {
-//         await openPage({ page, filter })
+    function testSelectPage({ pageNumber = 1, filter }: PaginationTestOptions) {
+      test(`should load correct items when selecting a page${
+        filter ? ' and is filtered' : ''
+      }`, async ({ page }) => {
+        await openPage({ page, filter })
 
-//         await clickVisiblePageButton(page, pageNumber)
-//         await page.waitForSelector(rowLoadedSelector)
+        await clickVisiblePageButton(page, pageNumber)
+        await page.waitForSelector(rowLoadedSelector)
 
-//         await waitForPageNumber(page, pageNumber)
-//         await validateTable({
-//           client,
-//           page,
-//           pageNumber,
-//           params: getParamsFromFilter(filter),
-//         })
-//       })
-//     }
+        await waitForPageNumber(page, pageNumber)
+      })
+    }
 
-//     function testSelectPageFromDropdown({
-//       filter,
-//       pageNumber = 1,
-//     }: PaginationTestOptions) {
-//       test(`should load correct items when selecting a page from dropdown${
-//         filter ? ' and is filtered' : ''
-//       }`, async ({ page }) => {
-//         await openPage({ page, filter })
+    function testSelectPageFromDropdown({
+      filter,
+      pageNumber = 1,
+    }: PaginationTestOptions) {
+      test(`should load correct items when selecting a page from dropdown${
+        filter ? ' and is filtered' : ''
+      }`, async ({ page }) => {
+        await openPage({ page, filter })
 
-//         await openPaginationDropdown(page)
-//         await selectPaginationDropdownOption(page, pageNumber)
-//         await page.waitForSelector(rowLoadedSelector)
+        await openPaginationDropdown(page)
+        await selectPaginationDropdownOption(page, pageNumber)
+        await page.waitForSelector(rowLoadedSelector)
 
-//         await waitForPageNumber(page, pageNumber)
-//         await validateTable({
-//           client,
-//           page,
-//           pageNumber,
-//           params: getParamsFromFilter(filter),
-//         })
-//       })
-//     }
+        await waitForPageNumber(page, pageNumber)
+      })
+    }
 
-//     testMaxPageCount()
-//     testMaxPageCount({ filter: testFilter })
+    testMaxPageCount()
+    testMaxPageCount({ filter: testFilter })
 
-//     testSelectPage({ pageNumber: pagination.selectPage })
-//     testSelectPage({ pageNumber: pagination.selectPage, filter: testFilter })
+    testSelectPage({ pageNumber: pagination.selectPage })
+    testSelectPage({ pageNumber: pagination.selectPage, filter: testFilter })
 
-//     testSelectPageFromDropdown({
-//       pageNumber: pagination.selectPageFromDropdown,
-//     })
-//     testSelectPageFromDropdown({
-//       pageNumber: pagination.selectPageFromDropdown,
-//       filter: testFilter,
-//     })
+    testSelectPageFromDropdown({
+      pageNumber: pagination.selectPageFromDropdown,
+    })
+    testSelectPageFromDropdown({
+      pageNumber: pagination.selectPageFromDropdown,
+      filter: testFilter,
+    })
 
-//     test('should load next page when clicking on next button', async ({
-//       page,
-//     }) => {
-//       await openPage({ page })
-//       await getNextButton(page).click()
-//       await page.waitForSelector(rowLoadedSelector)
-//       await waitForPageNumber(page, 2)
-//       await validateTable({
-//         client,
-//         page,
-//         pageNumber: 2,
-//       })
-//     })
+    test('should load next page when clicking on next button', async ({
+      page,
+    }) => {
+      await openPage({ page })
+      await getNextButton(page).click()
+      await page.waitForSelector(rowLoadedSelector)
+      await waitForPageNumber(page, 2)
+    })
 
-//     test('should load previous page when clicking on previous button', async ({
-//       page,
-//     }) => {
-//       await openPage({ page, pageNumber: 2 })
-//       await getPreviousButton(page).click()
-//       await page.waitForSelector(rowLoadedSelector)
-//       await waitForPageNumber(page, 1)
-//       await validateTable({
-//         client,
-//         page,
-//       })
-//     })
+    test('should load previous page when clicking on previous button', async ({
+      page,
+    }) => {
+      await openPage({ page, pageNumber: 2 })
+      await getPreviousButton(page).click()
+      await page.waitForSelector(rowLoadedSelector)
+      await waitForPageNumber(page, 1)
+    })
 
-//     test('should disable previous button when on first page', async ({
-//       page,
-//     }) => {
-//       await openPage({ page })
-//       await expect(getPreviousButton(page)).toHaveAttribute('disabled')
-//     })
+    test('should disable previous button when on first page', async ({
+      page,
+    }) => {
+      await openPage({ page })
+      await expect(getPreviousButton(page)).toHaveAttribute('disabled')
+    })
 
-//     test('should disable next button when on last page', async ({ page }) => {
-//       const maxPages = await getMaxPages(client)
-//       await openPage({ page, pageNumber: maxPages })
-//       await expect(getNextButton(page)).toHaveAttribute('disabled')
-//     })
+    test('should disable next button when on last page', async ({ page }) => {
+      const maxPages = await getMaxPages(client)
+      await openPage({ page, pageNumber: maxPages })
+      await expect(getNextButton(page)).toHaveAttribute('disabled')
+    })
 
-//     test('should hide pagination when maxPages is 1', async ({ page }) => {
-//       await openPage({ page, filter: testFilterWithNoPages })
-//       await expect(getPagination(page)).toBeHidden()
-//     })
-//   })
-// }
+    test('should hide pagination when maxPages is 1', async ({ page }) => {
+      await openPage({ page, filter: testFilterWithNoPages })
+      await expect(getPagination(page)).toBeHidden()
+    })
+  })
+}
