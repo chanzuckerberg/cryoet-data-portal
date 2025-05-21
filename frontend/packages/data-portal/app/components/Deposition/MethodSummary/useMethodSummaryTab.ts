4c4295ad70ce38776ea249296c108290086b3a5e
Jeremy <jeremyasuncion808@gmail.com>
2025-05-21 14:50:09 -0700

    descriptive query param

--- frontend/packages/data-portal/app/components/Deposition/MethodSummary/useMethodSummaryTab.ts
+++ frontend/packages/data-portal/app/components/Deposition/MethodSummary/useMethodSummaryTab.ts
@@ -6,7 +6,7 @@ import { MethodSummaryTab } from './types'
 const DEFAULT_ANNOTATION_TAB = MethodSummaryTab.Annotations
 
 export function useMethodSummaryTab() {
-  return useQueryParam<MethodSummaryTab>(QueryParams.Tab, {
+  return useQueryParam<MethodSummaryTab>(QueryParams.MethodSummaryTab, {
     defaultValue: DEFAULT_ANNOTATION_TAB,
   })
 }
