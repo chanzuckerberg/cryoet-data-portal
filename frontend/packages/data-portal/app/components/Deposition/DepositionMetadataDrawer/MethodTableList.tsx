633383e4ec085ae687b97844f9ed2c526be57e1d
Jeremy <jeremyasuncion808@gmail.com>
2025-05-22 16:56:37 -0700

    table spacing

--- frontend/packages/data-portal/app/components/Deposition/DepositionMetadataDrawer/MethodTableList.tsx
+++ frontend/packages/data-portal/app/components/Deposition/DepositionMetadataDrawer/MethodTableList.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from 'react'
-
 import { Accordion } from 'app/components/Accordion'
 import { MetadataTable } from 'app/components/Table'
 import { useI18n } from 'app/hooks/useI18n'
@@ -22,29 +20,31 @@ export function MethodTableList<T>({
 
   return (
     <Accordion header={t(header)} id={accordionId} initialOpen>
-      {data.map((d, index) => {
-        const tableData = getTableData(d)
+      <div className="flex flex-col gap-3.5">
+        {data.map((d, index) => {
+          const tableData = getTableData(d)
 
-        return (
-          // safe to use index as key since list never changes
-          // eslint-disable-next-line react/no-array-index-key
-          <Fragment key={`method-table-${index}`}>
-            <div className="flex items-center gap-[10px]">
-              <p
-                className={cns(
-                  'uppercase !text-sds-caps-xxxs-600-wide leading-sds-caps-xxxs',
-                  'font-semibold text-light-sds-color-primitive-gray-600',
-                )}
-              >
-                {t('methodCount', { value: index + 1 })}
-              </p>
-              <div className="flex-grow h-[1px] bg-light-sds-color-primitive-gray-300" />
+          return (
+            // safe to use index as key since list never changes
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={`method-table-${index}`}>
+              <div className="flex items-center gap-[10px]">
+                <p
+                  className={cns(
+                    'uppercase !text-sds-caps-xxxs-600-wide leading-sds-caps-xxxs',
+                    'font-semibold text-light-sds-color-primitive-gray-600',
+                  )}
+                >
+                  {t('methodCount', { value: index + 1 })}
+                </p>
+                <div className="flex-grow h-[1px] bg-light-sds-color-primitive-gray-300" />
+              </div>
+
+              <MetadataTable data={tableData} invertRowColor />
             </div>
-
-            <MetadataTable data={tableData} invertRowColor />
-          </Fragment>
-        )
-      })}
+          )
+        })}
+      </div>
     </Accordion>
   )
 }
