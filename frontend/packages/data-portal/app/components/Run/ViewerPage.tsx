f2ab7a5e8ef085d0e0666bfe94fe0a9db1c8d38b
Sean Martin <martins7@tcd.ie>
2025-05-30 15:55:37 +0200

    fix: remove # inclusion in ID name

--- frontend/packages/data-portal/app/components/Run/ViewerPage.tsx
+++ frontend/packages/data-portal/app/components/Run/ViewerPage.tsx
@@ -466,7 +466,7 @@ function ViewerPage({ run, tomogram }: { run: any; tomogram: any }) {
                             {depositions?.[0].annotation.deposition.title}
                           </span>
                           <span className="text-xs text-[#767676] font-normal">
-                            #CZCDP-{depositionId}
+                            CZCDP-{depositionId}
                           </span>
                         </CustomDropdownOption>
                       )
