743e8503c29a70c699a7a77e9560a097641ec6f6
Jeremy <jeremyasuncion808@gmail.com>
2025-06-05 14:46:23 -0700

    deposition id + data types filter

--- frontend/packages/data-portal/app/components/DepositionsFilters/DepositionsFilters.tsx
+++ frontend/packages/data-portal/app/components/DepositionsFilters/DepositionsFilters.tsx
@@ -2,6 +2,7 @@ import { QueryParams } from 'app/constants/query'
 import { useFilter } from 'app/hooks/useFilter'
 import { useFeatureFlag } from 'app/utils/featureFlags'
 
+import { DepositionIdFilter } from '../DepositionFilter'
 import { ErrorBoundary } from '../ErrorBoundary'
 import {
   AuthorFilter,
@@ -9,13 +10,17 @@ import {
   FilterPanel,
   FilterSection,
 } from '../Filters'
+import { DataTypesFilter } from '../Filters/DataTypesFilter'
 
 export function DepositionsFilters() {
   const showCompetitionFilter = useFeatureFlag('postMlChallenge')
+  const isExpandDepositions = useFeatureFlag('expandDepositions')
+
   const {
     updateValue,
     tags: { competition },
   } = useFilter()
+
   const filters = [
     {
       logId: 'competition-filter',
@@ -37,6 +42,19 @@ export function DepositionsFilters() {
       logId: 'author-filter',
       filter: <AuthorFilter label="Deposition Author" />,
     },
+
+    ...(isExpandDepositions
+      ? [
+          {
+            logId: 'deposition-id-filter',
+            filter: <DepositionIdFilter />,
+          },
+          {
+            logId: 'data-types-filter',
+            filter: <DataTypesFilter />,
+          },
+        ]
+      : []),
   ]
 
   return (
