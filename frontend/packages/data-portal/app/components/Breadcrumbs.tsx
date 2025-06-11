451bf07215bbc94b8b01a7a315f6f39cfa71152d
Sean Martin <martins7@tcd.ie>
2025-06-11 16:57:07 +0200

    feat: use responsive sizes in header bar

--- frontend/packages/data-portal/app/components/Breadcrumbs.tsx
+++ frontend/packages/data-portal/app/components/Breadcrumbs.tsx
@@ -170,13 +170,13 @@ export function Breadcrumbs({
 
   const buildNeuroglancerBreadcrumb = () => {
     const neuroglancerChevronIcon = (
-      <SmallChevronRightIcon className="w-[8px] h-[8px] shrink-0 text-[#999] fill-[#999]" />
+      <SmallChevronRightIcon className="hidden lg:block w-[8px] h-[8px] shrink-0 text-[#999] fill-[#999]" />
     )
     return (
       <div className="flex flex-row gap-sds-s text-dark-sds-color-primitive-gray-500 fill-[#999] font-normal text-sds-body-s-400-wide leading-sds-body-s items-center whitespace-nowrap content-start">
         <Tooltip
           tooltip={`Go to Dataset ${data.title || t('dataset')}`}
-          className="overflow-hidden overflow-ellipsis"
+          className="truncate max-w-0 lg:max-w-[10rem] xl:max-w-[16rem] 2xl:max-w-xl"
         >
           <Breadcrumb
             text={data.title || t('dataset')}
@@ -210,8 +210,8 @@ export function Breadcrumbs({
   return (
     <div
       className={cns(
-        variant === 'neuroglancer' && 'max-w-xl',
-        'flex flex-col flex-auto gap-1',
+        'flex-col flex-auto gap-1',
+        variant === 'neuroglancer' ? 'hidden md:flex' : 'flex',
       )}
       data-testid={TestIds.Breadcrumbs}
     >
