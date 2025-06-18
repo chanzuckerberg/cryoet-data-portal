3a07c68ed7344949414bb55308f5da674fafe900
Jeremy <jeremyasuncion808@gmail.com>
2025-06-18 09:28:41 -0700

    extract data contents query to fragment

--- frontend/packages/data-portal/app/components/Dataset/utils.ts
+++ frontend/packages/data-portal/app/components/Dataset/utils.ts
@@ -1,4 +1,5 @@
 import { UnfilteredRun } from 'app/types/gql/datasetPageTypes'
+import { getDataContents } from 'app/utils/deposition'
 
 export interface SummaryData {
   annotations: number
@@ -10,47 +11,16 @@ export interface SummaryData {
 }
 
 export const getContentSummaryCounts = (runs: UnfilteredRun[]): SummaryData => {
-  let annotations = 0
-  let tomograms = 0
-  let framesAvailable = false
-  let tiltSeriesAvailable = false
-  let ctfAvailable = false
-  let alignmentAvailable = false
-
-  for (const run of runs) {
-    annotations +=
-      run.annotationsAggregate?.aggregate?.reduce(
-        (sum, agg) => sum + (agg?.count || 0),
-        0,
-      ) || 0
-    tomograms +=
-      run.tomogramsAggregate?.aggregate?.reduce(
-        (sum, agg) => sum + (agg?.count || 0),
-        0,
-      ) || 0
-    framesAvailable ||=
-      run.framesAggregate?.aggregate?.some((agg) => (agg?.count || 0) > 0) ||
-      false
-    tiltSeriesAvailable ||=
-      run.tiltseriesAggregate?.aggregate?.some(
-        (agg) => (agg?.count || 0) > 0,
-      ) || false
-    ctfAvailable ||=
-      run.perSectionParametersAggregate?.aggregate?.some(
-        (agg) => (agg?.count || 0) > 0,
-      ) || false
-    alignmentAvailable ||=
-      run.alignmentsAggregate?.aggregate?.some(
-        (agg) => (agg?.count || 0) > 0,
-      ) || false
-  }
+  const dataContents = getDataContents(runs)
 
   return {
-    annotations,
-    tomograms,
-    frames: framesAvailable ? 'Available' : 'Not Submitted',
-    tiltSeries: tiltSeriesAvailable ? 'Available' : 'Not Submitted',
-    ctf: ctfAvailable ? 'Available' : 'Not Submitted',
-    alignment: alignmentAvailable ? 'Available' : 'Not Submitted',
+    annotations: dataContents.annotations,
+    tomograms: dataContents.tomograms,
+    frames: dataContents.framesAvailable ? 'Available' : 'Not Submitted',
+    tiltSeries: dataContents.tiltSeriesAvailable
+      ? 'Available'
+      : 'Not Submitted',
+    ctf: dataContents.ctfAvailable ? 'Available' : 'Not Submitted',
+    alignment: dataContents.alignmentAvailable ? 'Available' : 'Not Submitted',
   }
 }
