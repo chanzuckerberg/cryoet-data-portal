a99df5131bb6526bd5baf01016147bf56b15497a
Sean Martin <martins7@tcd.ie>
2025-06-23 16:49:35 +0200

    refactor: rework how menu styles are done

--- frontend/packages/data-portal/app/components/MenuDropdown.tsx
+++ frontend/packages/data-portal/app/components/MenuDropdown.tsx
@@ -13,11 +13,44 @@ export type MenuDropdownRef = {
   closeMenu: () => void
 }
 
+const MENU_STYLES = {
+  button: {
+    standard: {
+      default: '!p-0',
+    },
+    outlined: {
+      default: 'border px-3.5 py-1.5 rounded-full',
+      active: 'border-white',
+      inactive: 'border-black',
+    },
+  },
+  text: {
+    standard: {
+      active: 'text-light-sds-color-primitive-gray-50',
+      inactive: 'text-light-sds-color-primitive-gray-400',
+    },
+    outlined: {
+      active: 'text-black',
+      inactive: 'text-dark-sds-color-primitive-gray-600',
+    },
+  },
+  icon: {
+    standard: {
+      active: '!fill-light-sds-color-primitive-gray-50',
+      inactive: '!fill-light-sds-color-primitive-gray-400',
+    },
+    outlined: {
+      active: '!fill-black',
+      inactive: '!fill-dark-sds-color-primitive-gray-600',
+    },
+  },
+}
+
 export const MenuDropdown = forwardRef<
   MenuDropdownRef,
   {
     children: ReactNode
-    variant?: 'standard' | 'outlined' | 'filled'
+    variant?: 'standard' | 'outlined'
     className?: string
     title: ReactNode
     buttonElement?: ReactNode
@@ -42,42 +75,35 @@ export const MenuDropdown = forwardRef<
       closeMenu: () => setAnchorEl(null),
     }))
 
-    const variantStyles = {
-      standard: '!p-0',
-      outlined: 'border border-[#A2C9FF] px-3.5 py-1.5 rounded-full',
-      filled: 'bg-[#2573F4] px-3.5 py-1.5 rounded-full',
-    }
-
-    const iconStyles = {
-      standard: anchorEl
-        ? '!text-light-sds-color-primitive-gray-400'
-        : '!text-light-sds-color-primitive-gray-400 group-hover:!text-light-sds-color-primitive-gray-50',
-      outlined: '!text-[#A2C9FF]',
-      filled: '!fill-sds-color-primitive-common-white',
-    }
-
-    const textStyles = {
-      standard: anchorEl
-        ? 'text-light-sds-color-primitive-gray-400'
-        : 'text-light-sds-color-primitive-gray-400 group-hover:text-light-sds-color-primitive-gray-50',
-      outlined: '!text-[#A2C9FF]',
-      filled: '!fill-sds-color-primitive-common-white',
-    }
-
     return (
-      <div className={className}>
+      <div className={cns(className, 'group')}>
         {title ? (
           <button
             className={cns(
-              'flex items-center gap-2 group',
-              variantStyles[variant],
+              'flex items-center gap-2',
+              MENU_STYLES.button[variant].default,
+              variant === 'outlined' &&
+                (anchorEl
+                  ? MENU_STYLES.button[variant].active
+                  : cns(
+                      MENU_STYLES.button[variant].inactive,
+                      `group-hover:${MENU_STYLES.button[variant].active}`,
+                    )),
             )}
             onClick={() => setAnchorEl(menuRef.current)}
             type="button"
           >
             <span
               ref={menuRef}
-              className={cns('font-semibold', textStyles[variant])}
+              className={cns(
+                'font-semibold',
+                anchorEl
+                  ? MENU_STYLES.text[variant].active
+                  : cns(
+                      MENU_STYLES.text[variant].inactive,
+                      `group-hover:${MENU_STYLES.text[variant].active}`,
+                    ),
+              )}
             >
               {title}
             </span>
@@ -85,7 +111,15 @@ export const MenuDropdown = forwardRef<
             <Icon
               sdsIcon={anchorEl ? 'ChevronUp' : 'ChevronDown'}
               sdsSize="xs"
-              className={cns(iconStyles[variant], '!w-[10px] !h-[10px]')}
+              className={cns(
+                '!w-[10px] !h-[10px]',
+                anchorEl
+                  ? MENU_STYLES.icon[variant].active
+                  : cns(
+                      MENU_STYLES.icon[variant].inactive,
+                      `group-hover:${MENU_STYLES.icon[variant].active}`,
+                    ),
+              )}
             />
           </button>
         ) : (
