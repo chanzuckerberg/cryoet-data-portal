94f4bfa7cfddd05a227d0b37ce4e1809e4bbce43
Jeremy <jeremyasuncion808@gmail.com>
2025-08-04 10:07:34 -0700

    update dates

--- frontend/packages/data-portal/app/components/PolicyBanner/PolicyBanner.tsx
+++ frontend/packages/data-portal/app/components/PolicyBanner/PolicyBanner.tsx
@@ -2,7 +2,7 @@ import { LocalStorageKeys } from 'app/constants/localStorage'
 
 import { ReusableBanner } from '../ReusableBanner/ReusableBanner'
 
-const POLICY_BANNER_END_DATE = '2025-09-01'
+const POLICY_BANNER_END_DATE = '2025-09-03'
 
 export function PolicyBanner() {
   return (
