3bbe444134d49593971f4a12417997c25c10fb7d
Jeremy <jeremyasuncion808@gmail.com>
2025-10-08 12:46:52 -0700

    fix object name filter

--- frontend/packages/data-portal/e2e/pageObjects/filters/filtersPage.ts
+++ frontend/packages/data-portal/e2e/pageObjects/filters/filtersPage.ts
@@ -46,6 +46,10 @@ export class FiltersPage extends BasePage {
     }
   }
 
+  public async clickObjectNameDropdown() {
+    await this.page.getByRole('button', { name: 'Dropdown input' }).click()
+  }
+
   public async selectFilterOption(label: string) {
     await this.page
       .getByRole('option', { name: label })
@@ -59,6 +63,14 @@ export class FiltersPage extends BasePage {
       .locator('span', { hasText: new RegExp(`^${escapeRegExp(label)}$`, 'i') })
       .locator('..')
       .getByLabel('Delete tag')
+      .click()
+  }
+
+  public async removeObjectNameFilter(label: string) {
+    await this.page
+      .locator('span', { hasText: new RegExp(`^${escapeRegExp(label)}$`, 'i') })
+      .locator('..')
+      .getByLabel('remove-filter')
       .click()
   }
 
