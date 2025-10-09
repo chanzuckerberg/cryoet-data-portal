c6f2e24b4867766eca6447db94c75361031d34cf
Jeremy <jeremyasuncion808@gmail.com>
2025-10-08 18:34:58 -0700

    more code cleanup

--- frontend/packages/data-portal/app/types/gql/depositionPageTypes.ts
+++ frontend/packages/data-portal/app/types/gql/depositionPageTypes.ts
@@ -1,7 +1,3 @@
-import {
-  GetDatasetsV2Query,
-  GetDepositionBaseDataV2Query,
-} from 'app/__generated_v2__/graphql'
+import { GetDepositionBaseDataV2Query } from 'app/__generated_v2__/graphql'
 
-export type Dataset = GetDatasetsV2Query['datasets'][number]
 export type Deposition = GetDepositionBaseDataV2Query['depositions'][number]
