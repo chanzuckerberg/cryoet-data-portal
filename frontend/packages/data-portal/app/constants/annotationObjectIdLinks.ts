c72ed6bcf533e7e9a2f56071fc686ba0d6ce7b21
PALLAVIKHEDLE <pallavi8khedle@gmail.com>
2026-03-13 12:42:26 -0700

    docs: add CDPO ontology reference page and update CDPO link

--- frontend/packages/data-portal/app/constants/annotationObjectIdLinks.ts
+++ frontend/packages/data-portal/app/constants/annotationObjectIdLinks.ts
@@ -2,7 +2,8 @@ export const GO = 'https://amigo.geneontology.org/amigo/term/'
 export const UNIPROTKB = 'https://www.uniprot.org/uniprotkb/'
 export const CHEBI = 'http://purl.obolibrary.org/obo/CHEBI_'
 export const UBERON = 'http://purl.obolibrary.org/obo/UBERON_'
-export const CDPO = 'https://github.com/uermel/cdpo'
+export const CDPO =
+  'https://chanzuckerberg.github.io/cryoet-data-portal/cdpo.html'
 
 export const GO_PREFIX = 'GO:'
 export const UNIPROTKB_PREFIX = 'UniProtKB:'
