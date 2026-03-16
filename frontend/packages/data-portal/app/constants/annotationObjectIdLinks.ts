0bf715a2db162fdefce0dd3f13725733d3ddb9d4
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2026-03-16 11:42:59 -0700

    fix: Use separate learn more link for object ID filter (#2040)
    * fix: Use separate learn more link for object ID filter
    
    * CDPO link changes
    
    * chore: Updated [rdev] values.yaml image tags to sha-bf5a545
    
    ---------
    
    Co-authored-by: czi-github-helper[bot] <+czi-github-helper[bot]@users.noreply.github.com>

--- frontend/packages/data-portal/app/constants/annotationObjectIdLinks.ts
+++ frontend/packages/data-portal/app/constants/annotationObjectIdLinks.ts
@@ -3,10 +3,17 @@ export const UNIPROTKB = 'https://www.uniprot.org/uniprotkb/'
 export const CHEBI = 'http://purl.obolibrary.org/obo/CHEBI_'
 export const UBERON = 'http://purl.obolibrary.org/obo/UBERON_'
 export const CDPO =
-  'https://chanzuckerberg.github.io/cryoet-data-portal/cdpo.html'
+  'https://chanzuckerberg.github.io/cryoet-data-portal/stable/cdpo.html'
 
 export const GO_PREFIX = 'GO:'
 export const UNIPROTKB_PREFIX = 'UniProtKB:'
 export const CHEBI_PREFIX = 'CHEBI:'
 export const UBERON_PREFIX = 'UBERON:'
 export const CDPO_PREFIX = 'CDPO:'
+
+export const GO_LEARN_MORE = 'https://geneontology.org/'
+export const UNIPROTKB_LEARN_MORE = 'https://www.uniprot.org/uniprotkb/'
+export const CHEBI_LEARN_MORE = 'https://www.ebi.ac.uk/chebi/'
+export const UBERON_LEARN_MORE = 'https://obophenotype.github.io/uberon/'
+export const CDPO_LEARN_MORE =
+  'https://chanzuckerberg.github.io/cryoet-data-portal/stable/cdpo.html'
